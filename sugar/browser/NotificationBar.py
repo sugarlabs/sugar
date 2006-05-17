de8ef992942e2809649e81c2b523a80164f9cd21
Marco Pesenti Gritti <mpg@redhat.com>
2006-05-17 17:04:42 -0400

    Forgot to add the file

---
+++ sugar/browser/NotificationBar.py
@@ -0,0 +1,30 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+
+class NotificationBar(gtk.HBox):
+	__gsignals__ = {
+		'action': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE,
+				  ([gobject.TYPE_STRING]))
+	}
+
+	def __init__(self):
+		gtk.HBox.__init__(self)
+		self._text_label = gtk.Label()
+		self.pack_start(self._text_label)
+		self._text_label.show()
+		
+		self._action_button = gtk.Button()
+		self.pack_start(self._action_button, False)
+		self._action_button.show()
+	
+	def set_text(self, text):
+		self._text_label.set_text(text)
+	
+	def set_action(self, action_id, action_text):
+		self._action_id = action_id
+		self._action_button.set_label(action_text)
+		
+	def __action_button_clicked(self):
+		self.emit("action", self._action_id)
