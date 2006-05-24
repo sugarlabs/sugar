b4ebdfa5af05da701eee3a68b89ee77dc47b002f
Marco Pesenti Gritti <mpg@redhat.com>
2006-05-23 20:03:35 -0400

    Initialize profile

--- examples/gmail.py
+++ examples/gmail.py
@@ -1,15 +1,20 @@
+import os
+
 import pygtk
 pygtk.require('2.0')
 import gtk
 import geckoembed
 
 from sugar.shell import activity
+import sugar.env
 
 class GMailActivity(activity.Activity):
 	def __init__(self):
 		activity.Activity.__init__(self)
 	
 	def activity_on_connected_to_shell(self):
+		profile_path = os.path.join(sugar.env.get_user_dir(), 'gmail')
+		geckoembed.set_profile_path(profile_path)
 		self.activity_set_tab_text("Mail")
 		self.activity_set_tab_icon_name("stock_mail")
 		self.activity_show_icon(True)
