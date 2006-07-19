1590e15c2569a3d1478286d21027d43a79d973da
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-07-19 20:59:46 +0200

    Fix path building

--- sugar/__uninstalled__.py
+++ sugar/__uninstalled__.py
@@ -3,4 +3,4 @@ import os
 _source_dir = os.path.dirname(os.path.dirname(__file__))
 
 sugar_data_dir = os.path.join(_source_dir, 'shell/data') 
-sugar_activities_dir = os.path.join(sugar_data_dir, 'activities')
+sugar_activities_dir = os.path.join(_source_dir, 'activities')
