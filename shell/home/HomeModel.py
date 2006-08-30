adb269709c51601f8c55a71594e2274a8fa9ad64
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-08-30 10:55:57 +0200

    Remove obsolete import

--- shell/home/HomeModel.py
+++ shell/home/HomeModel.py
@@ -1,10 +1,8 @@
 from home.FriendsModel import FriendsModel
-from home.MeshModel import MeshModel
 
 class HomeModel:
 	def __init__(self):
 		self._friends = FriendsModel()
-		self._mesh = MeshModel()
 
 	def get_friends(self):
 		return self._friends
