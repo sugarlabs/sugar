26c50e3b3dc98a16572686c015818b7769ab0ea0
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-08-31 10:10:45 +0200

    Panels doesn't take focus

--- shell/panel/Panel.py
+++ shell/panel/Panel.py
@@ -38,6 +38,7 @@ class Panel(gtk.Window):
 
 		self.realize()
 		self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+		self.window.set_accept_focus(False)
 
 		screen = gtk.gdk.screen_get_default()
 		self.window.set_transient_for(screen.get_root_window())
