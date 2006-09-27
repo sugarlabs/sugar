906f5bbed0c2199d738f6f4b0b28a398afa8054b
Dan Williams <dcbw@localhost.localdomain>
2006-09-27 17:00:12 -0400

    Make ShellModel emit signals on activity change, and make Owner listen for them.  Fix Owner's current activity update code to actually update at the correct interval

--- shell/model/ShellModel.py
+++ shell/model/ShellModel.py
@@ -2,15 +2,22 @@ from sugar.presence import PresenceService
 from model.Friends import Friends
 from model.MeshModel import MeshModel
 from model.Owner import ShellOwner
+import gobject
 
-class ShellModel:
+class ShellModel(gobject.GObject):
+	__gsignals__ = {
+		'activity-changed':  (gobject.SIGNAL_RUN_FIRST,
+							 gobject.TYPE_NONE, ([gobject.TYPE_PYOBJECT]))
+	}
+
 	def __init__(self):
+		gobject.GObject.__init__(self)
 		self._current_activity = None
 
 		PresenceService.start()
 		self._pservice = PresenceService.get_instance()
 
-		self._owner = ShellOwner()
+		self._owner = ShellOwner(self)
 		self._owner.announce()
 		self._friends = Friends()
 		self._mesh = MeshModel()
@@ -29,7 +36,7 @@ class ShellModel:
 
 	def set_current_activity(self, activity_id):
 		self._current_activity = activity_id
-		self._owner.set_current_activity(activity_id)
+		self.emit('activity-changed', activity_id)
 
 	def get_current_activity(self):
 		return self._current_activity
