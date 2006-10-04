5da630c13a4ec45b96fc42ba3b8ea1c9c1712be5
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-10-04 15:37:53 +0200

    Kill unused code, fix makefiles

--- sugar/canvas/CanvasView.py
+++
@@ -1,9 +0,0 @@
-import gtk
-import goocanvas
-
-class CanvasView(goocanvas.CanvasView):
-	def __init__(self):
-		goocanvas.CanvasView.__init__(self)
-
-		self.set_bounds(0, 0, 1200, 900)
-		self.set_scale(gtk.gdk.screen_width() / 1200.0)
