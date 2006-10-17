0836c57f76ad4c7c375c6d99ded0925d97c3d691
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-10-17 10:14:42 +0200

    Get rid of the global python path extensions

--- sugar/env.py
+++ sugar/env.py
@@ -26,17 +26,7 @@ except ImportError:
 
 import sugar.setup
 
-def setup_python_path():
-	for path in sugar_python_path:
-		if os.environ.has_key('PYTHONPATH'):
-			old_path = os.environ['PYTHONPATH']
-			os.environ['PYTHONPATH'] = path + ':' + old_path 
-		else:
-			os.environ['PYTHONPATH'] = path
-
 def setup_system():
-	setup_python_path()
-
 	for path in sugar_bin_path:
 		if os.environ.has_key('PATH'):
 			old_path = os.environ['PATH']
