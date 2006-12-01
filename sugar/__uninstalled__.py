833e7633e9b5962dadc6cf72ad66105682a20cce
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-12-01 21:25:14 +0100

    Leftover

--- sugar/__uninstalled__.py
+++ sugar/__uninstalled__.py
@@ -8,5 +8,4 @@ sugar_data_dir = os.path.join(_sourcedir, 'shell/data')
 sugar_services_dir = os.path.join(_sourcedir, 'services')
 sugar_activity_info_dir = _tmpdir
 sugar_activities_dir = os.path.join(_sourcedir, 'activities')
-sugar_dbus_config = os.path.join(_sourcedir, 'dbus-uninstalled.conf')
 sugar_shell_bin_dir = os.path.join(_sourcedir, 'shell')
