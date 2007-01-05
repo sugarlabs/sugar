81b56491b64dac5d1a7588d1032ae5c60adab188
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-01-05 12:59:01 +0100

    Crappy script to build a snapshot.

---
+++ tools/build-snapshot.sh
@@ -0,0 +1,11 @@
+VERSION=0.63
+DATE=`date +%Y%m%d`
+RELEASE=1
+TARBALL=sugar-$VERSION.$RELEASE.$DATE.tar.bz2
+
+rm sugar-$VERSION.tar.bz2
+make distcheck
+
+mv sugar-$VERSION.tar.bz2 $TARBALL
+scp $TARBALL mpg@devserv.devel.redhat.com:~
+rm $TARBALL
