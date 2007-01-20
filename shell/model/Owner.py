d36e20159e298ba2dbc138a5369bfa32f0a3bce1
Marco Pesenti Gritti <mpg@redhat.com>
2007-01-20 13:46:32 +0100

    Our xmlrpc implementation does not work with python 2.5. Try/except it and log an error.

--- shell/model/Owner.py
+++ shell/model/Owner.py
@@ -74,9 +74,13 @@ class ShellOwner(object):
         self._service = self._pservice.register_service(self._nick,
                 PRESENCE_SERVICE_TYPE, properties=props)
         logging.debug("Owner '%s' using port %d" % (self._nick, self._service.get_port()))
-        self._icon_stream = Stream.Stream.new_from_service(self._service)
-        self._icon_stream.register_reader_handler(self._handle_buddy_icon_request, "get_buddy_icon")
-        self._icon_stream.register_reader_handler(self._handle_invite, "invite")
+
+        try:
+            self._icon_stream = Stream.Stream.new_from_service(self._service)
+            self._icon_stream.register_reader_handler(self._handle_buddy_icon_request, "get_buddy_icon")
+            self._icon_stream.register_reader_handler(self._handle_invite, "invite")
+        except:
+            logging.error('Our xmlprc implementation does not work with Python 2.5')
 
     def _handle_buddy_icon_request(self):
         """XMLRPC method, return the owner's icon encoded with base64."""
