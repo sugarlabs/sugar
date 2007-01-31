8f50955479285ebf5a79e441ee4a7e1eb68d85c0
Marco Pesenti Gritti <mpg@redhat.com>
2007-01-31 14:02:22 -0500

    Pass --with-libxul-sdk

--- tools/build-snapshot.sh
+++ tools/build-snapshot.sh
@@ -1,10 +1,12 @@
 VERSION=0.63
 DATE=`date +%Y%m%d`
-RELEASE=2.15
+RELEASE=2.16
 TARBALL=sugar-$VERSION-$RELEASE.${DATE}git.tar.bz2
 
 rm sugar-$VERSION.tar.bz2
-make distcheck
+
+XUL_SDK=/home/marco/sugar-jhbuil/build/lib/xulrunner-1.9a2
+DISTCHECK_CONFIGURE_FLAGS="--with-xul-sdk=$XUL_SDK" make distcheck
 
 mv sugar-$VERSION.tar.bz2 $TARBALL
 scp $TARBALL mpg@devserv.devel.redhat.com:~
