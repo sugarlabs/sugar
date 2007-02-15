67c9268a753f4e73ddc480462633b03ee28f9397
Marco Pesenti Gritti <mpg@redhat.com>
2007-02-15 18:55:32 +0100

    Take in account screen size

--- sugar/graphics/units.py
+++ sugar/graphics/units.py
@@ -3,7 +3,8 @@ import gtk
 import _sugar
 
 def points_to_pixels(points):
-    return points * _sugar.get_screen_dpi() / 72.0
+    screen_factor = gtk.gdk.screen_width() / 1200.0
+    return points * _sugar.get_screen_dpi() / 72.0 * screen_factor
 
 def grid_to_pixels(units):
     return units * gtk.gdk.screen_width() / 16
