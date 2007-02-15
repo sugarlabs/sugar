fa4c775bc9b0647c5e5a075ecc7b98067ebf7584
Marco Pesenti Gritti <mpg@redhat.com>
2007-02-15 19:03:24 +0100

    Add icon sizes

--- sugar/graphics/units.py
+++ sugar/graphics/units.py
@@ -2,9 +2,16 @@ import gtk
 
 import _sugar
 
+_screen_factor = gtk.gdk.screen_width() / 1200.0
+
+STANDARD_ICON_SCALE = 1.0 * _screen_factor
+SMALL_ICON_SCALE    = 0.5 * _screen_factor
+MEDIUM_ICON_SCALE   = 1.5 * _screen_factor
+LARGE_ICON_SCALE    = 2.0 * _screen_factor
+XLARGE_ICON_SCALE   = 3.0 * _screen_factor
+
 def points_to_pixels(points):
-    screen_factor = gtk.gdk.screen_width() / 1200.0
-    return points * _sugar.get_screen_dpi() / 72.0 * screen_factor
+    return points * _sugar.get_screen_dpi() / 72.0 * _screen_factor
 
 def grid_to_pixels(units):
     return units * gtk.gdk.screen_width() / 16
