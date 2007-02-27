b3452663bc3cda80bf83b45385027897574696a9
Marco Pesenti Gritti <mpg@redhat.com>
2007-02-27 20:08:33 +0100

    Use a custom style to get rid of the damned window background

--- sugar/graphics/window.py
+++ sugar/graphics/window.py
@@ -1,18 +1,24 @@
 import hippo
 import gtk
 
+from sugar.graphics import color
+
+class _Style(gtk.Style):
+    __gtype_name__ = 'SugarCanvasStyle'
+    def __init__(self):
+        gtk.Style.__init__(self)
+
+    def do_set_background(self, window, state):
+        window.set_back_pixmap(None, False)
+
 class Window(gtk.Window):
     def __init__(self, window_type=gtk.WINDOW_TOPLEVEL):
         gtk.Window.__init__(self, window_type)
 
         self._canvas = hippo.Canvas()
+        self._canvas.set_style(_Style())
         self.add(self._canvas)
         self._canvas.show()
 
-        self._canvas.connect_after('realize', self._window_realize_cb)
-
     def set_root(self, root):
         self._canvas.set_root(root)
-
-    def _window_realize_cb(self, canvas):
-        canvas.window.set_back_pixmap(None, False)
