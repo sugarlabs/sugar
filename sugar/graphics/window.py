2e2db562c5faa21e623707ebe84e5e504df540d0
Marco Pesenti Gritti <mpg@redhat.com>
2007-02-27 13:56:00 +0100

    Make the canvas window background transparent

--- sugar/graphics/window.py
+++ sugar/graphics/window.py
@@ -9,6 +9,10 @@ class Window(gtk.Window):
         self.add(self._canvas)
         self._canvas.show()
 
+        self._canvas.connect_after('realize', self._window_realize_cb)
+
     def set_root(self, root):
         self._canvas.set_root(root)
 
+    def _window_realize_cb(self, canvas):
+        canvas.window.set_back_pixmap(None, False)
