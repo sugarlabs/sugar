33739f1c0f0fe8bcf72bb7bf65a18ee9c2dbf169
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2007-03-02 22:21:45 +0100

    Some more examples for the journal.

--- services/datastore/demodata.py
+++ services/datastore/demodata.py
@@ -59,6 +59,28 @@ def insert_demo_data(data_store):
                                 'color' : '#C2B00C,#785C78' },
                               { 'name'  : 'Eben', 
                                 'color' : '#75C228,#3A6E3A' } ])
+        },
+        {   'file-path' : os.path.join(journal_dir, 'thai_story.abw'),
+            'object-type' : 'text',
+            'date'    : str(time.time() - 450000),
+            'title'   : 'Thai history',
+            'preview' : "The history of Thailand begins with the migration of the Thais from their ancestoral home in southern China into mainland southeast asia around the 10th century AD.",
+            'icon'    : 'theme:object-text',
+            'icon-color' : '#C2B00C,#785C78',
+            'keep'    : '1',
+            'buddies' : str([ { 'name'  : 'Marco',
+                                'color' : '#C2B00C,#785C78' } ])
+        },
+        {   'file-path' : os.path.join(journal_dir, 'thai_prince.abw'),
+            'object-type' : 'text',
+            'date'    : str(time.time() - 450000),
+            'title'   : 'The Thai Prince',
+            'preview' : "Prince Dipangkara Rasmijoti of Thailand, (born 29 April 2005), is a member of the Thailand Royal Family, a grandson of King Bhumibol Adulyadej (Rama IX) of Thailand is the fifth son of Maha Vajiralongkorn, Crown Prince of Thailand.",
+            'icon'    : 'theme:object-text',
+            'icon-color' : '#C2B00C,#785C78',
+            'keep'    : '0',
+            'buddies' : str([ { 'name'  : 'Eben', 
+                                'color' : '#75C228,#3A6E3A' } ])
         }
     ]
     for obj in data:
