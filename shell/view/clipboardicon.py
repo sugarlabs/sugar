93e9d164c0ada98212a37da2c17bd640261561bb
Dan Williams <dcbw@redhat.com>
2007-03-14 14:43:42 -0400

    Auto-open some downloaded files

--- shell/view/clipboardicon.py
+++ shell/view/clipboardicon.py
@@ -96,11 +96,17 @@ class ClipboardIcon(CanvasIcon):
         else:
             self.props.xo_color = XoColor("#000000,#FFFFFF")
 
+        if activity and percent == 100:
+            # FIXME: restrict based on file type rather than activity once
+            # we have a better type registry
+            # restrict auto-open to a specific set of activities
+            allowed = ["org.laptop.AbiWordActivity", "org.laptop.sugar.Xbook"]
+            if activity in allowed:
+                self._open_file()
+
     def _open_file(self):
         if self._percent < 100 or not self._activity:
             return
-
-        logging.debug("_open_file: " + self._object_id)
 
         # Get the file path
         cb_service = clipboardservice.get_instance()
