71b0a5dc56aa3884af1c25043230143696bb74ec
Marco Pesenti Gritti <mpg@redhat.com>
2007-03-16 10:48:40 +0100

    Actually pass buttons to the gtk file picker

--- sugar/graphics/filechooser.py
+++ sugar/graphics/filechooser.py
@@ -5,6 +5,6 @@ from sugar.graphics import units
 class FileChooserDialog(gtk.FileChooserDialog):
     def __init__(self, title=None, parent=None,
                  action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=None):
-        gtk.FileChooserDialog.__init__(self)
-        self.resize(units.points_to_pixels(7 * 40),
-                    units.points_to_pixels(7 * 30))
+        gtk.FileChooserDialog.__init__(self, title, parent, action, buttons)
+        self.set_default_size(units.points_to_pixels(7 * 40),
+                              units.points_to_pixels(7 * 30))
