96243824bbccecd2bfe3b19e09dd7e6daf16621d
Marco Pesenti Gritti <mpg@redhat.com>
2007-03-19 15:20:44 +0100

    Remove our own sizing

--- sugar/graphics/filechooser.py
+++ sugar/graphics/filechooser.py
@@ -6,5 +6,3 @@ class FileChooserDialog(gtk.FileChooserDialog):
     def __init__(self, title=None, parent=None,
                  action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=None):
         gtk.FileChooserDialog.__init__(self, title, parent, action, buttons)
-        self.set_default_size(units.points_to_pixels(7 * 40),
-                              units.points_to_pixels(7 * 30))
