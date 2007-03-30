ca3b37c660b0adb21d12e350fc12dc931151307d
Marco Pesenti Gritti <mpg@redhat.com>
2007-03-30 16:18:26 +0200

    Use xulrunner

--- tools/build-snapshot.sh
+++ tools/build-snapshot.sh
@@ -1,12 +1,12 @@
 VERSION=0.63
 DATE=`date +%Y%m%d`
-RELEASE=2.67
+RELEASE=2.69
 TARBALL=sugar-$VERSION-$RELEASE.${DATE}git.tar.bz2
 
 rm sugar-$VERSION.tar.bz2
 
-XUL_SDK=/home/marco/sugar-jhbuil/build/lib/xulrunner-1.9a2
-DISTCHECK_CONFIGURE_FLAGS="--with-xul-sdk=$XUL_SDK" make distcheck
+XUL_SDK=/home/marco/sugar-jhbuild/build/lib/xulrunner-1.9a4pre-dev
+DISTCHECK_CONFIGURE_FLAGS="--with-libxul-sdk=$XUL_SDK" make distcheck
 
 mv sugar-$VERSION.tar.bz2 $TARBALL
 scp $TARBALL mpg@devserv.devel.redhat.com:~
