92416d1519c67012e02af17a672edead91f68011
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-04-17 18:17:26 +0200

    Fix typo in the fallback code path

--- shell/model/homemodel.py
+++ shell/model/homemodel.py
@@ -166,7 +166,7 @@ class HomeModel(gobject.GObject):
         else:
             # activity got lost, took longer to launch than we allow,
             # or it was launched by something other than the shell
-            act_type = act_service.get_service_name()
+            act_type = service.get_service_name()
             bundle = self._bundle_registry.get_bundle(act_type)
             if not bundle:
                 raise RuntimeError("No bundle for activity type '%s'." % act_type)
