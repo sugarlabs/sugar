2baa2881e05b4f49109d072e02f0de420bfa82cd
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-04-27 11:47:02 +0200

    Kill more canvas items

--- sugar/graphics/filechooser.py
+++
@@ -1,8 +0,0 @@
-import gtk
-
-from sugar.graphics import units
-
-class FileChooserDialog(gtk.FileChooserDialog):
-    def __init__(self, title=None, parent=None,
-                 action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=None):
-        gtk.FileChooserDialog.__init__(self, title, parent, action, buttons)
