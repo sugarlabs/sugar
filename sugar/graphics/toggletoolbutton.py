e78c9d21a9070a4bcfd5f3ab7fcabc4542808e75
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2007-04-30 19:00:34 +0200

    Added ToggleToolButton control.

---
+++ sugar/graphics/toggletoolbutton.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2007, Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import gtk
+
+from sugar.graphics.icon import Icon
+
+class ToggleToolButton(gtk.ToggleToolButton):
+    def __init__(self, icon_resource=None):
+        gtk.ToggleToolButton.__init__(self)
+        
+        icon = Icon(icon_resource)
+        self.set_icon_widget(icon)
+        icon.show()
