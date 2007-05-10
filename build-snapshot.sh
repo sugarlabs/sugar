cb1119a6478c5938fd61055c179f6e4bfcf4dd39
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-05-10 18:55:29 +0200

    Fix several distcheck issues

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -1,11 +1,11 @@
 VERSION=0.63
 DATE=`date +%Y%m%d`
-RELEASE=2.72
+RELEASE=2.76
 TARBALL=sugar-$VERSION-$RELEASE.${DATE}git.tar.bz2
 
 rm sugar-$VERSION.tar.bz2
 
-XUL_SDK=/home/marco/sugar-jhbuild/build/lib/xulrunner-1.9a4pre-dev
+XUL_SDK=/home/marco/sugar-jhbuild/build/lib/xulrunner-1.9a5pre-dev
 DISTCHECK_CONFIGURE_FLAGS="--with-libxul-sdk=$XUL_SDK" make distcheck
 
 mv sugar-$VERSION.tar.bz2 $TARBALL
