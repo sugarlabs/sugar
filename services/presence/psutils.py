a868f6eb9c8de4855b8462be71be4e305af4e510
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-05-25 11:30:36 +0100

    services/presence/psutils: Re-order imports conventionally, without dbus.glib. Importing dbus.glib has a side-effect (it sets up the default main loop integration) so it's not something a module should do.

--- services/presence/psutils.py
+++ services/presence/psutils.py
@@ -14,8 +14,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import dbus, dbus.glib, gobject
 import logging
+
+import dbus
+import gobject
 
 
 _logger = logging.getLogger('s-p-s.psutils')
