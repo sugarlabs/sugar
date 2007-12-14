ac804177a11aee4a16fa82087ff5a06a916bf2a3
Benjamin Berg <benjamin@sipsolutions.net>
2007-12-14 20:44:22 +0100

    Extend the iconwidget test to also test insensitive icons.

--- tests/graphics/iconwidget.py
+++ tests/graphics/iconwidget.py
@@ -28,24 +28,60 @@ import common
 
 test = common.Test()
 
-icon = Icon(icon_name='go-previous')
-icon.props.icon_size = gtk.ICON_SIZE_LARGE_TOOLBAR
-test.pack_start(icon)
-icon.show()
+hbox = gtk.HBox()
+test.pack_start(hbox)
+sensitive_box = gtk.VBox()
+insensitive_box = gtk.VBox()
 
-icon = Icon(icon_name='computer-xo', 
-            icon_size=gtk.ICON_SIZE_LARGE_TOOLBAR,
-            xo_color=XoColor())
-test.pack_start(icon)
-icon.show()
+hbox.pack_start(sensitive_box)
+hbox.pack_start(insensitive_box)
+hbox.show_all()
 
-icon = Icon(icon_name='battery-000',
-            icon_size=gtk.ICON_SIZE_LARGE_TOOLBAR,
-            badge_name='badge-busy')
-test.pack_start(icon)
-icon.show()
+
+def create_icon_widgets(box, sensitive=True):
+    icon = Icon(icon_name='go-previous')
+    icon.props.icon_size = gtk.ICON_SIZE_LARGE_TOOLBAR
+    box.pack_start(icon)
+    icon.set_sensitive(sensitive)
+    icon.show()
+
+    icon = Icon(icon_name='computer-xo', 
+                icon_size=gtk.ICON_SIZE_LARGE_TOOLBAR,
+                xo_color=XoColor())
+    box.pack_start(icon)
+    icon.set_sensitive(sensitive)
+    icon.show()
+
+    icon = Icon(icon_name='battery-000',
+                icon_size=gtk.ICON_SIZE_LARGE_TOOLBAR,
+                badge_name='emblem-busy')
+    box.pack_start(icon)
+    icon.set_sensitive(sensitive)
+    icon.show()
+
+    icon = Icon(icon_name='gtk-new',
+                icon_size=gtk.ICON_SIZE_LARGE_TOOLBAR,
+                badge_name='gtk-cancel')
+    box.pack_start(icon)
+    icon.set_sensitive(sensitive)
+    icon.show()
+
+
+create_icon_widgets(sensitive_box, True)
+create_icon_widgets(insensitive_box, False)
 
 test.show()
+
+# This can be used to test for leaks by setting the LRU cache size
+# in icon.py to 1.
+#def idle_cb():
+#    import gc
+#    gc.collect()
+#    test.queue_draw()
+#    return True
+#
+#import gobject
+#gobject.idle_add(idle_cb)
 
 if __name__ == "__main__":
     common.main(test)
