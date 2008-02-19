3677745a10fbc7481fa1da02bd7b4c63e28d0deb
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2008-02-19 16:07:58 +0100

    Add palettes to the icons for the running activities.

--- src/view/frame/activitiestray.py
+++ src/view/frame/activitiestray.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2007 Red Hat, Inc.
+# Copyright (C) 2008 One Laptop Per Child
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,14 +15,75 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import logging
+from gettext import gettext as _
+
+import gtk
 
 from sugar.graphics.tray import HTray
 from sugar.graphics.radiotoolbutton import RadioToolButton
 from sugar.graphics.icon import Icon
+from sugar.graphics.palette import Palette
+from sugar.graphics.menuitem import MenuItem
+
+from view.frame.frameinvoker import FrameWidgetInvoker
+
+class ActivityPalette(Palette):
+    def __init__(self, home_activity):
+        Palette.__init__(self, home_activity.get_title())
+        self.props.invoker = FrameWidgetInvoker(self)
+        self.set_group_id('frame')
+
+        self._home_activity = home_activity
+
+        menu_item = MenuItem(_('Resume'), 'activity-start')
+        menu_item.connect('activate', self.__resume_activate_cb)
+        self.menu.append(menu_item)
+        menu_item.show()
+
+        menu_item = MenuItem(_('Share with'), 'zoom-neighborhood')
+        #menu_item.connect('activate', self.__share_activate_cb)
+        self.menu.append(menu_item)
+        menu_item.show()
+
+        menu_item = MenuItem(_('Stop'), 'activity-stop')
+        menu_item.connect('activate', self.__stop_activate_cb)
+        self.menu.append(menu_item)
+        menu_item.show()
+
+    def __resume_activate_cb(self, menu_item):
+        self._home_activity.get_window().activate(1)
+
+    def __stop_activate_cb(self, menu_item):
+        self._home_activity.get_window().close(1)
+
+class JournalPalette(Palette):
+    def __init__(self, home_activity):
+        Palette.__init__(self, home_activity.get_title())
+        self.props.invoker = FrameWidgetInvoker(self)
+        self.set_group_id('frame')
+
+        self._home_activity = home_activity
+
+        menu_item = MenuItem(_('Open Journal'))
+
+        icon = Icon(file=home_activity.get_icon_path(),
+                icon_size=gtk.ICON_SIZE_MENU,
+                xo_color=home_activity.get_icon_color())
+        menu_item.set_image(icon)
+        icon.show()
+
+        menu_item.connect('activate', self.__open_activate_cb)
+        self.menu.append(menu_item)
+        menu_item.show()
+
+    def __open_activate_cb(self, menu_item):
+        self._home_activity.get_window().activate(1)
 
 class ActivityButton(RadioToolButton):
     def __init__(self, home_activity, group):
         RadioToolButton.__init__(self, group=group)
+
+        self._home_activity = home_activity
 
         icon = Icon(xo_color=home_activity.get_icon_color())
         if home_activity.get_icon_path():
@@ -31,6 +92,29 @@ class ActivityButton(RadioToolButton):
             icon.props.icon_name = 'image-missing'
         self.set_icon_widget(icon)
         icon.show()
+
+        if home_activity.props.launching:
+            palette = Palette(_('Starting...'))
+            palette.props.invoker = FrameWidgetInvoker(self)
+            palette.set_group_id('frame')
+            self.set_palette(palette)
+
+            #self._start_pulsing()
+            home_activity.connect('notify::launching', self._launching_changed_cb)
+        else:
+            self._setup_palette()
+
+    def _launching_changed_cb(self, home_activity, pspec):
+        if not home_activity.props.launching:
+            #self._stop_pulsing()
+            self._setup_palette()
+
+    def _setup_palette(self):
+        if self._home_activity.get_type() == "org.laptop.JournalActivity":
+            palette = JournalPalette(self._home_activity)
+        else:
+            palette = ActivityPalette(self._home_activity)
+        self.set_palette(palette)
 
 class ActivitiesTray(HTray):
     def __init__(self, shell):
@@ -53,8 +137,7 @@ class ActivitiesTray(HTray):
         button = ActivityButton(home_activity, group)
         self.add_item(button)
         self._buttons[home_activity.get_activity_id()] = button
-        button.connect('toggled', self.__activity_toggled_cb,
-                home_activity.get_activity_id())
+        button.connect('toggled', self.__activity_toggled_cb, home_activity)
         button.show()
 
     def __activity_removed_cb(self, home_model, home_activity):
@@ -68,8 +151,6 @@ class ActivitiesTray(HTray):
         button = self._buttons[home_activity.get_activity_id()]
         button.props.active = True
 
-    def __activity_toggled_cb(self, button, activity_id):
-        activity_host = self._shell.get_activity(activity_id)
-        if activity_host:
-            activity_host.present()
+    def __activity_toggled_cb(self, button, home_activity):
+        home_activity.get_window().activate(1)
 
