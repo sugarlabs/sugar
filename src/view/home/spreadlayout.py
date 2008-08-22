65d2cba836a02cc23591e8f433ca5e618e30abb6
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2008-08-22 10:19:59 +0200

    #7660 Correctly set the icon allocation

--- src/view/home/spreadlayout.py
+++ src/view/home/spreadlayout.py
@@ -22,7 +22,7 @@ from sugar.graphics import style
 
 from view.home.grid import Grid
 
-_CELL_SIZE = 4
+_CELL_SIZE = 4.0
 
 class SpreadLayout(gobject.GObject, hippo.CanvasLayout):
     __gtype_name__ = 'SugarSpreadLayout'
@@ -66,10 +66,10 @@ class SpreadLayout(gobject.GObject, hippo.CanvasLayout):
             min_h, child_height = child.get_height_request(child_width)
 
             rect = self._grid.get_child_rect(child.item)
-            child.allocate(rect.x * _CELL_SIZE,
-                           rect.y * _CELL_SIZE,
-                           rect.width * _CELL_SIZE,
-                           rect.height * _CELL_SIZE,
+            child.allocate(int(round(rect.x * _CELL_SIZE)),
+                           int(round(rect.y * _CELL_SIZE)),
+                           child_width,
+                           child_height,
                            origin_changed)
 
     def _get_child_grid_size(self, child):
