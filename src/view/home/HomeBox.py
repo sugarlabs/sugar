bcd9a73f436894fea8ebc0f2b168d7c525d42b35
Simon Schampijer <simon@schampijer.de>
2008-08-29 14:35:19 +0200

    Fix clearing of search entry in Home (#7874)
    Without this fix, the list view fails to return to an unfiltered state
    when then entry is cleared from the favorites view. (eben)

--- src/view/home/HomeBox.py
+++ src/view/home/HomeBox.py
@@ -294,10 +294,11 @@ class HomeToolbar(gtk.Toolbar):
         if self._autosearch_timer:
             gobject.source_remove(self._autosearch_timer)
         new_query = entry.props.text
-        if self._query != new_query and new_query is not '':
+        if self._query != new_query:
             self._query = new_query
 
-            self._list_button.props.active = True
+            if self._query is not '':
+                self._list_button.props.active = True
             self.emit('query-changed', self._query)
 
     def __entry_changed_cb(self, entry):
