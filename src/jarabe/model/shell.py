6c64746c43262cd1269bc04e17d3261fe7abf552
Marco Pesenti Gritti <mpgritti@gmail.com>
2008-10-18 15:54:13 +0200

    Better check for desktop vs activity zoom level.

--- src/jarabe/model/shell.py
+++ src/jarabe/model/shell.py
@@ -322,10 +322,10 @@ class ShellModel(gobject.GObject):
         self._screen.toggle_showing_desktop(True)
 
     def _update_zoom_level(self, window):
-        if window.get_window_type() == wnck.WINDOW_DESKTOP:
-            zoom_level = self._desktop_level
-        else:
+        if window.get_window_type() == wnck.WINDOW_NORMAL:
             zoom_level = self.ZOOM_ACTIVITY
+        else:
+            zoom_level = self._desktop_level
 
         if self._zoom_level != zoom_level:
             self._zoom_level = zoom_level
