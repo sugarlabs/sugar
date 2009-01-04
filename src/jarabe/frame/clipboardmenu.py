4aca69f9c96620411b3cd7db0da2306ca87a0103
Tomeu Vizoso <tomeu@sugarlabs.org>
2009-01-04 15:18:21 +0100

    #131 Properly retrieve the default activity

--- src/jarabe/frame/clipboardmenu.py
+++ src/jarabe/frame/clipboardmenu.py
@@ -172,7 +172,7 @@ class ClipboardMenu(Palette):
         if percent < 100 or menu_item.get_submenu() is not None:
             return
         jobject = self._copy_to_journal()
-        misc.resume(jobject.metadata, self._activities[0])
+        misc.resume(jobject.metadata, self._get_activities()[0])
         jobject.destroy()
 
     def _open_submenu_item_activate_cb(self, menu_item, service_name):
