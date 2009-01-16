653de31eed0619c0e7356c45d8932ac4c9574fc5
Simon Schampijer <simon@schampijer.de>
2009-01-16 13:45:49 +0100

    separate debug settings from xsession #163
    - source the debug options in the sugar executable
    - create ~/.sugar/debug file on profile creation where
    one can customize the debug options

--- src/jarabe/intro/__init__.py
+++ src/jarabe/intro/__init__.py
@@ -11,6 +11,10 @@ from jarabe.intro.window import create_profile
 def check_profile():
     profile = get_profile()
 
+    path = os.path.join(os.path.expanduser('~/.sugar'), 'debug')
+    if not os.path.exists(path):
+        profile.create_debug_file()
+
     path = os.path.join(env.get_profile_path(), 'config')
     if os.path.exists(path):
         profile.convert_profile()
