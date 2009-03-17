afcc8e2e80786f70744057dbba570a36826b9b1d
Jonas Smedegaard <dr@jones.dk>
2009-03-17 10:19:36 +0100

    Consistently use underscore (not dash) in variables.

--- debian/cdbs/1/rules/package-relations.mk
+++ debian/cdbs/1/rules/package-relations.mk
@@ -21,8 +21,8 @@ _cdbs_scripts_path ?= /usr/lib/cdbs
 _cdbs_rules_path ?= /usr/share/cdbs/1/rules
 _cdbs_class_path ?= /usr/share/cdbs/1/class
 
-ifndef _cdbs_rules_package-relations
-_cdbs_rules_package-relations = 1
+ifndef _cdbs_rules_package_relations
+_cdbs_rules_package_relations = 1
 
 include $(_cdbs_rules_path)/buildcore.mk$(_cdbs_makefile_suffix)
 
