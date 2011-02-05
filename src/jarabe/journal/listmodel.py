sugarlabs/sugar

commit 79c85ed25efaa6eed02589467fa96145795c1205

Journal list view: don't choke on invalid or incomplete metadata (SL#1408)
Metadata can get corrupted by crashes or malformed by buggy activities.
We should do our best to display the parts that are usable and certainly
never mess up the entire Journal.

Acked-by: Simon Schampijer <simon@schampijer.de>

src/jarabe/journal/listmodel.py
@@ -140,11 +140,16 @@ class ListModel(gtk.GenericTreeModel, gtk.TreeDragSource):
             xo_color = misc.get_icon_color(metadata)
         self._cached_row.append(xo_color)
 
-        title = gobject.markup_escape_text(metadata.get('title', None))
-        self._cached_row.append('<b>%s</b>' % title)
+        title = gobject.markup_escape_text(metadata.get('title',
+                                           _('Untitled')))
+        self._cached_row.append('<b>%s</b>' % (title, ))
 
-        timestamp = int(metadata.get('timestamp', 0))
-        self._cached_row.append(util.timestamp_to_elapsed_string(timestamp))
+        try:
+            timestamp = float(metadata.get('timestamp', 0))
+        except (TypeError, ValueError):
+            self._cached_row.append(_('Unknown'))
+        else:
+            self._cached_row.append(util.timestamp_to_elapsed_string(timestamp))
 
         try:
             creation_time = float(metadata.get('creation_time'))
@@ -161,19 +166,37 @@ class ListModel(gtk.GenericTreeModel, gtk.TreeDragSource):
         else:
             self._cached_row.append(util.format_size(size))
 
-        self._cached_row.append(int(metadata.get('progress', 100)))
+        try:
+            progress = int(float(metadata.get('progress', 100)))
+        except (TypeError, ValueError):
+            progress = 100
+        self._cached_row.append(progress)
 
-        if metadata.get('buddies', ''):
-            buddies = simplejson.loads(metadata['buddies']).values()
-        else:
+        buddies = []
+        if metadata.get('buddies'):
+            try:
+                buddies = simplejson.loads(metadata['buddies']).values()
+            except simplejson.decoder.JSONDecodeError, exception:
+                logging.warning('Cannot decode buddies for %r: %s',
+                                metadata['uid'], exception)
+
+        if not isinstance(buddies, list):
+            logging.warning('Content of buddies for %r is not a list: %r',
+                            metadata['uid'], buddies)
             buddies = []
 
         for n_ in xrange(0, 3):
             if buddies:
-                nick, color = buddies.pop(0)
-                self._cached_row.append((nick, XoColor(color)))
-            else:
-                self._cached_row.append(None)
+                try:
+                    nick, color = buddies.pop(0)
+                except (AttributeError, ValueError), exception:
+                    logging.warning('Malformed buddies for %r: %s',
+                                    metadata['uid'], exception)
+                else:
+                    self._cached_row.append((nick, XoColor(color)))
+                    continue
+
+            self._cached_row.append(None)
 
         return self._cached_row[column]
 

commit 9b047d21923e34bf1d3dc1155ad83106e2acc5d4

fix stylistic mistake introduced by 79c85ed2
Signed-off-by: Sascha Silbe <silbe@activitycentral.com>
Reviewed-by: Aleksey Lim <alsroot@activitycentral.org>

src/jarabe/journal/listmodel.py
@@ -147,9 +147,10 @@ class ListModel(gtk.GenericTreeModel, gtk.TreeDragSource):
         try:
             timestamp = float(metadata.get('timestamp', 0))
         except (TypeError, ValueError):
-            self._cached_row.append(_('Unknown'))
+            timestamp_content = _('Unknown')
         else:
-            self._cached_row.append(util.timestamp_to_elapsed_string(timestamp))
+            timestamp_content = util.timestamp_to_elapsed_string(timestamp)
+        self._cached_row.append(timestamp_content)
 
         try:
             creation_time = float(metadata.get('creation_time'))