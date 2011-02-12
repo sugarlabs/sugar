sugarlabs/sugar

commit 4b6a534e6b60c01f2021edb91faf039008294b4b

Based on Jame's comment at http://dev.laptop.org/ticket/9189#comment:25 I am reverting this commit.
Revert "restore sugar-launch by bundle id substring, fixes #897"

This reverts commit 4a3416b9acdfdd533ee2d69633f1383476db5e71.

src/jarabe/model/bundleregistry.py
@@ -174,22 +174,12 @@ class BundleRegistry(gobject.GObject):
         self._write_favorites_file()
 
     def get_bundle(self, bundle_id):
-        """Returns a bundle given service name or substring,
-        returns None if there is either no match, or more than one
-        match by substring."""
-        result = []
-        key = bundle_id.lower()
-
+        """Returns an bundle given his service name"""
         for bundle in self._bundles:
-            name = bundle.get_bundle_id()
-            if name == bundle_id:
+            if bundle.get_bundle_id() == bundle_id:
                 return bundle
-            if key in name.lower():
-                result.append(bundle)
-        if len(result) == 1:
-            return result[0]
         return None
-
+    
     def __iter__(self):
         return self._bundles.__iter__()
 

commit fe8065054e2ab3d79b6ce27a56e97b4a977620a8

fix whitespace error introduced by 4b6a534 (revert of 4a3416b)
Signed-off-by: Sascha Silbe <silbe@activitycentral.com>
Reviewed-by: Aleksey Lim <alsroot@activitycentral.org>

src/jarabe/model/bundleregistry.py
@@ -179,7 +179,7 @@ class BundleRegistry(gobject.GObject):
             if bundle.get_bundle_id() == bundle_id:
                 return bundle
         return None
-    
+
     def __iter__(self):
         return self._bundles.__iter__()
 