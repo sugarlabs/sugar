5cfb315d89eaa2fb21372f4fb545315bb28fb1e0
Simon Schampijer <simon@schampijer.de>
2011-05-25 17:39:18 +0200

    Convert nick to a str, follow up on OLPC #10735
    Commit dad5ad7f4477b84d15705129cd7e97bdbe9457fe did not
    solve the issue for non-english systems.
    
    Signed-off-by: Simon Schampijer <simon@laptop.org>
    Acked-By: Sascha Silbe <silbe@activitycentral.com>

--- src/jarabe/frame/activitiestray.py
+++ src/jarabe/frame/activitiestray.py
@@ -545,7 +545,7 @@ class IncomingTransferPalette(BaseTransferPalette):
 
         self.file_transfer.connect('notify::state', self.__notify_state_cb)
 
-        nick = self.file_transfer.buddy.props.nick
+        nick = str(self.file_transfer.buddy.props.nick)
         self.props.secondary_text = _('Transfer from %s') % (nick,)
 
         self._update()
@@ -676,7 +676,7 @@ class OutgoingTransferPalette(BaseTransferPalette):
 
         self.file_transfer.connect('notify::state', self.__notify_state_cb)
 
-        nick = file_transfer.buddy.props.nick
+        nick = str(file_transfer.buddy.props.nick)
         self.props.secondary_text = _('Transfer to %s') % (nick,)
 
         self._update()
