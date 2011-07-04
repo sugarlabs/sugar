sugarlabs/sugar

commit 9d0d3720c167e1897fe5b9d055d1763710c507b8

Raise alert when trying to send an entry without an associated file OLPC #10798
We raise the same error when we try to copy an entry without an
associated file to an external device.

Signed-off-by: Simon Schampijer <simon@laptop.org>
Acked-By: Sascha Silbe <silbe@activitycentral.com>

src/jarabe/journal/palettes.py
@@ -143,6 +143,13 @@ class ObjectPalette(Palette):
         logging.debug('__friend_selected_cb')
         file_name = model.get_file(self._metadata['uid'])
 
+        if not file_name or not os.path.exists(file_name):
+            logging.warn('Entries without a file cannot be sent.')
+            self.emit('volume-error',
+                      _('Entries without a file cannot be sent.'),
+                      _('Warning'))
+            return
+
         title = str(self._metadata['title'])
         description = str(self._metadata.get('description', ''))
         mime_type = str(self._metadata['mime_type'])

commit 077521c35f21270033408e09518bf003fb6b25ec

Add missing import (follow up of OLPC #10798)
This is a fix for a regression introduced by commit
9d0d372 ("Raise alert when trying to send an entry without an associated
file OLPC #10798").

Signed-off-by: Simon Schampijer <simon@laptop.org>
Acked-By: Sascha Silbe <silbe@activitycentral.com>

src/jarabe/journal/palettes.py
@@ -16,6 +16,7 @@
 
 from gettext import gettext as _
 import logging
+import os
 
 import gobject
 import gtk