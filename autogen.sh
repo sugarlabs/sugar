6fbc4188249a6f59cc55381824cf1ec1f90dc5e8
Daniel Narvaez <dwnarvaez@gmail.com>
2012-06-26 14:26:02 +0200

    Allow to build outside the source directory
    This is based on a patch by
    Marco Pesenti Gritti <marco@marcopg.org>,
    with reviewer comments addressed.
    
    Reviewed-by: James Cameron <quozl@laptop.org>
    Acked-by: Simon Schampijer <simon@schampijer.de>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+test -n "${srcdir}" || srcdir=`dirname "$0"`
+test -n "${srcdir}" || srcdir="$(pwd)"
+
+olddir="$(pwd)"
+cd "$srcdir"
+
 intltoolize
 autoreconf -i
-./configure --enable-maintainer-mode "$@"
+
+cd "$olddir"
+"$srcdir/configure" --enable-maintainer-mode "$@"
