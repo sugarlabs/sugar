sugarlabs/sugar

commit 798548f856b8ada8a41cd700e1ed0f2c79a8643f

Palettes: adjust to the new API - SL #4144
The API now allows to hide the implementation details using a
PaletteMenuBox as container.

Signed-off-by: Manuel Quiñones <manuq@laptop.org>
Acked-by: Simon Schampijer <simon@laptop.org>

src/jarabe/view/palettes.py
@@ -25,8 +25,9 @@ from gi.repository import Gtk
 
 from sugar3 import env
 from sugar3.graphics.palette import Palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.menuitem import MenuItem
 from sugar3.graphics.icon import Icon
 from sugar3.graphics import style
@@ -83,25 +84,25 @@ class CurrentActivityPalette(BasePalette):
         if title and title != activity_name:
             self.props.secondary_text = glib.markup_escape_text(title)
 
-        self.menu_box = Gtk.VBox()
+        self.menu_box = PaletteMenuBox()
 
         menu_item = PaletteMenuItem(_('Resume'), 'activity-start')
         menu_item.connect('activate', self.__resume_activate_cb)
-        self.menu_box.pack_start(menu_item, True, True, 0)
+        self.menu_box.append_item(menu_item)
 
         # TODO: share-with, keep
 
         menu_item = PaletteMenuItem(_('View Source'), 'view-source')
         menu_item.connect('activate', self.__view_source__cb)
-        self.menu_box.pack_start(menu_item, True, True, 0)
+        self.menu_box.append_item(menu_item)
 
         separator = PaletteMenuItemSeparator()
-        self.menu_box.pack_start(menu_item, True, True, 0)
+        self.menu_box.append_item(menu_item)
         separator.show()
 
         menu_item = PaletteMenuItem(_('Stop'), 'activity-stop')
         menu_item.connect('activate', self.__stop_activate_cb)
-        self.menu_box.pack_start(menu_item, True, True, 0)
+        self.menu_box.append_item(menu_item)
 
         self.set_content(self.menu_box)
         self.menu_box.show_all()
@@ -141,12 +142,13 @@ class ActivityPalette(Palette):
 
         xo_color = XoColor('%s,%s' % (style.COLOR_WHITE.get_svg(),
                                       style.COLOR_TRANSPARENT.get_svg()))
-        self.menu_box = Gtk.VBox()
+        self.menu_box = PaletteMenuBox()
         menu_item = PaletteMenuItem(text_label=_('Start new'),
                                     file_name=activity_info.get_icon(),
                                     xo_color=xo_color)
         menu_item.connect('activate', self.__start_activate_cb)
         self.menu_box.pack_end(menu_item, True, True, 0)
+        menu_item.show()
         self.set_content(self.menu_box)
         self.menu_box.show_all()
 
@@ -169,9 +171,9 @@ class JournalPalette(BasePalette):
         title = self._home_activity.get_title()
         self.set_primary_text(glib.markup_escape_text(title))
 
-        vbox = Gtk.VBox()
-        self.set_content(vbox)
-        vbox.show()
+        box = PaletteMenuBox()
+        self.set_content(box)
+        box.show()
 
         menu_item = PaletteMenuItem(_('Show contents'))
         icon = Icon(file=self._home_activity.get_icon_path(),
@@ -181,20 +183,24 @@ class JournalPalette(BasePalette):
         icon.show()
 
         menu_item.connect('activate', self.__open_activate_cb)
-        vbox.add(menu_item)
+        box.append_item(menu_item)
         menu_item.show()
 
         separator = PaletteMenuItemSeparator()
-        vbox.pack_start(separator, True, True, 0)
+        box.append_item(separator)
         separator.show()
 
+        inner_box = Gtk.VBox()
+        box.append_item(inner_box)
+        inner_box.show()
+
         self._progress_bar = Gtk.ProgressBar()
-        vbox.add(self._progress_bar)
+        inner_box.add(self._progress_bar)
         self._progress_bar.show()
 
         self._free_space_label = Gtk.Label()
         self._free_space_label.set_alignment(0.5, 0.5)
-        vbox.add(self._free_space_label)
+        inner_box.add(self._free_space_label)
         self._free_space_label.show()
 
         self.connect('popup', self.__popup_cb)
@@ -222,7 +228,7 @@ class VolumePalette(Palette):
         path = mount.get_root().get_path()
         self.props.secondary_text = glib.markup_escape_text(path)
 
-        self.content_box = Gtk.VBox()
+        self.content_box = PaletteMenuBox()
         self.set_content(self.content_box)
         self.content_box.show()
 
@@ -233,20 +239,20 @@ class VolumePalette(Palette):
         icon.show()
 
         menu_item.connect('activate', self.__unmount_activate_cb)
-        self.content_box.pack_start(menu_item, True, True, 0)
+        self.content_box.append_item(menu_item)
         menu_item.show()
 
         separator = PaletteMenuItemSeparator()
-        self.content_box.pack_start(separator, True, True, 0)
+        self.content_box.append_item(separator)
         separator.show()
 
         self._progress_bar = Gtk.ProgressBar()
-        self.content_box.pack_start(self._progress_bar, True, True, 0)
+        self.content_box.append_item(self._progress_bar)
         self._progress_bar.show()
 
         self._free_space_label = Gtk.Label()
         self._free_space_label.set_alignment(0.5, 0.5)
-        self.content_box.pack_start(self._free_space_label, True, True, 0)
+        self.content_box.append_item(self._free_space_label)
         self._free_space_label.show()
 
         self.connect('popup', self.__popup_cb)

extensions/deviceicon/battery.py
@@ -27,6 +27,7 @@ from sugar3.graphics import style
 from sugar3.graphics.icon import get_icon_state
 from sugar3.graphics.tray import TrayIcon
 from sugar3.graphics.palette import Palette
+from sugar3.graphics.palettemenu import PaletteMenuBox
 from sugar3.graphics.xocolor import XoColor
 
 from jarabe.frame.frameinvoker import FrameWidgetInvoker
@@ -116,20 +117,24 @@ class BatteryPalette(Palette):
         self._level = 0
         self._time = 0
         self._status = _STATUS_NOT_PRESENT
+
+        self._progress_widget = PaletteMenuBox()
+        self.set_content(self._progress_widget)
+        self._progress_widget.show()
+
+        inner_box = Gtk.VBox()
+        self._progress_widget.append_item(inner_box)
+        inner_box.show()
+
         self._progress_bar = Gtk.ProgressBar()
         self._progress_bar.set_size_request(
             style.zoom(style.GRID_CELL_SIZE * 4), -1)
+        inner_box.pack_start(self._progress_bar, True, True, 0)
         self._progress_bar.show()
+
         self._status_label = Gtk.Label()
+        inner_box.pack_start(self._status_label, True, True, 0)
         self._status_label.show()
-
-        vbox = Gtk.VBox()
-        vbox.pack_start(self._progress_bar, True, True, 0)
-        vbox.pack_start(self._status_label, True, True, 0)
-        vbox.show()
-
-        self._progress_widget = vbox
-        self.set_content(self._progress_widget)
 
     def set_info(self, percentage, seconds, status):
         self._level = percentage

extensions/deviceicon/network.py
@@ -34,8 +34,9 @@ import dbus
 from sugar3.graphics.icon import get_icon_state
 from sugar3.graphics import style
 from sugar3.graphics.palette import Palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.toolbutton import ToolButton
 from sugar3.graphics.tray import TrayIcon
 from sugar3.graphics.icon import Icon
@@ -74,6 +75,8 @@ class WirelessPalette(Palette):
         self._channel_label.show()
 
         self._ip_address_label = Gtk.Label()
+        self._ip_address_label.props.xalign = 0.0
+        self._ip_address_label.show()
 
         self._info = Gtk.VBox()
 

extensions/deviceicon/speaker.py
@@ -25,8 +25,9 @@ from sugar3.graphics import style
 from sugar3.graphics.icon import get_icon_state, Icon
 from sugar3.graphics.tray import TrayIcon
 from sugar3.graphics.palette import Palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.xocolor import XoColor
 
 from jarabe.frame.frameinvoker import FrameWidgetInvoker
@@ -90,19 +91,19 @@ class SpeakerPalette(Palette):
 
         self._model = model
 
-        vbox = Gtk.VBox()
-        self.set_content(vbox)
-        vbox.show()
+        box = PaletteMenuBox()
+        self.set_content(box)
+        box.show()
 
         self._mute_item = PaletteMenuItem('')
         self._mute_icon = Icon(icon_size=Gtk.IconSize.MENU)
         self._mute_item.set_image(self._mute_icon)
-        vbox.add(self._mute_item)
+        box.append_item(self._mute_item)
         self._mute_item.show()
         self._mute_item.connect('activate', self.__mute_activate_cb)
 
         separator = PaletteMenuItemSeparator()
-        vbox.pack_start(separator, True, True, 0)
+        box.append_item(separator)
         separator.show()
 
         vol_step = sound.VOLUME_STEP
@@ -112,11 +113,13 @@ class SpeakerPalette(Palette):
                                           step_incr=vol_step,
                                           page_incr=vol_step,
                                           page_size=vol_step)
-        self._hscale = Gtk.HScale()
-        self._hscale.set_adjustment(self._adjustment)
-        self._hscale.set_digits(0)
-        vbox.add(self._hscale)
-        self._hscale.show()
+
+        hscale = Gtk.HScale()
+        hscale.props.draw_value = False
+        hscale.set_adjustment(self._adjustment)
+        hscale.set_digits(0)
+        box.append_item(hscale)
+        hscale.show()
 
         self._adjustment_handler_id = \
             self._adjustment.connect('value_changed',

extensions/deviceicon/speech.py
@@ -24,8 +24,9 @@ from gi.repository import GObject
 from sugar3.graphics.icon import Icon
 from sugar3.graphics.tray import TrayIcon
 from sugar3.graphics.palette import Palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.xocolor import XoColor
 from sugar3.graphics import style
 
@@ -66,9 +67,9 @@ class SpeechPalette(Palette):
         self._manager.connect('stop', self._set_menu_state, 'stop')
         self._manager.connect('pause', self._set_menu_state, 'pause')
 
-        vbox = Gtk.VBox()
-        self.set_content(vbox)
-        vbox.show()
+        box = PaletteMenuBox()
+        self.set_content(box)
+        box.show()
 
         self._play_icon = Icon(icon_name='player_play')
         self._pause_icon = Icon(icon_name='player_pause')
@@ -77,48 +78,48 @@ class SpeechPalette(Palette):
             text_label=_('Say selected text'))
         self._play_pause_menu.set_image(self._play_icon)
         self._play_pause_menu.connect('activate', self.__play_activated_cb)
-        vbox.pack_start(self._play_pause_menu, True, True, 0)
+        box.append_item(self._play_pause_menu)
         self._play_pause_menu.show()
 
         self._stop_menu = PaletteMenuItem(icon_name='player_stop',
                                           text_label=_('Stop playback'))
         self._stop_menu.connect('activate', self.__stop_activated_cb)
         self._stop_menu.set_sensitive(False)
-        vbox.pack_start(self._stop_menu, True, True, 0)
-        self._stop_menu.show()
+        box.append_item(self._stop_menu)
 
         separator = PaletteMenuItemSeparator()
-        vbox.pack_start(separator, True, True, 0)
+        box.append_item(separator)
         separator.show()
 
         pitch_label = Gtk.Label(_('Pitch'))
-        vbox.pack_start(pitch_label, True, True, padding=style.DEFAULT_PADDING)
+        box.append_item(pitch_label, vertical_padding=0)
         pitch_label.show()
 
         self._adj_pitch = Gtk.Adjustment(value=self._manager.get_pitch(),
                                          lower=self._manager.MIN_PITCH,
                                          upper=self._manager.MAX_PITCH)
-        self._hscale_pitch = Gtk.HScale()
-        self._hscale_pitch.set_adjustment(self._adj_pitch)
-        self._hscale_pitch.set_draw_value(False)
 
-        vbox.pack_start(self._hscale_pitch, True, True, 0)
-        self._hscale_pitch.show()
+        hscale_pitch = Gtk.HScale()
+        hscale_pitch.set_adjustment(self._adj_pitch)
+        hscale_pitch.set_draw_value(False)
+
+        box.append_item(hscale_pitch, vertical_padding=0)
+        hscale_pitch.show()
 
         rate_label = Gtk.Label(_('Rate'))
-        vbox.pack_start(rate_label, True, True,
-                        padding=style.DEFAULT_PADDING)
+        box.append_item(rate_label, vertical_padding=0)
         rate_label.show()
 
         self._adj_rate = Gtk.Adjustment(value=self._manager.get_rate(),
                                         lower=self._manager.MIN_RATE,
                                         upper=self._manager.MAX_RATE)
-        self._hscale_rate = Gtk.HScale()
-        self._hscale_rate.set_adjustment(self._adj_rate)
-        self._hscale_rate.set_draw_value(False)
 
-        vbox.pack_start(self._hscale_rate, True, True, 0)
-        self._hscale_rate.show()
+        hscale_rate = Gtk.HScale()
+        hscale_rate.set_adjustment(self._adj_rate)
+        hscale_rate.set_draw_value(False)
+
+        box.append_item(hscale_rate, vertical_padding=0)
+        hscale_rate.show()
 
         self._adj_pitch.connect('value_changed', self.__adj_pitch_changed_cb)
         self._adj_rate.connect('value_changed', self.__adj_rate_changed_cb)

extensions/deviceicon/volume.py
@@ -24,8 +24,9 @@ from gi.repository import GConf
 
 from sugar3.graphics.tray import TrayIcon
 from sugar3.graphics.xocolor import XoColor
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.icon import Icon
 
 from jarabe.journal import journalactivity

src/jarabe/desktop/favoritesview.py
@@ -29,8 +29,9 @@ from gi.repository import GdkPixbuf
 from sugar3.graphics import style
 from sugar3.graphics.icon import Icon
 from sugar3.graphics.icon import CanvasIcon
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3.graphics.alert import Alert
 from sugar3.graphics.xocolor import XoColor
 from sugar3.activity import activityfactory

src/jarabe/desktop/networkviews.py
@@ -32,8 +32,9 @@ from sugar3.graphics import xocolor
 from sugar3.graphics import style
 from sugar3.graphics.icon import get_icon_state
 from sugar3.graphics import palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.palettemenuitem import PaletteMenuItemSeparator
+from sugar3.graphics.palettemenu import PaletteMenuBox
+from sugar3.graphics.palettemenu import PaletteMenuItem
+from sugar3.graphics.palettemenu import PaletteMenuItemSeparator
 from sugar3 import profile
 
 from jarabe.view.pulsingicon import EventPulsingIcon

commit 652979a535dc567594aadce85d522c9a54493e10

Fix separator in activity palette, regressed in 798548f8 - SL #4169
Signed-off-by: Manuel Quiñones <manuq@laptop.org>
Acked-by: Simon Schampijer <simon@laptop.org>

src/jarabe/view/palettes.py
@@ -97,7 +97,7 @@ class CurrentActivityPalette(BasePalette):
         self.menu_box.append_item(menu_item)
 
         separator = PaletteMenuItemSeparator()
-        self.menu_box.append_item(menu_item)
+        self.menu_box.append_item(separator)
         separator.show()
 
         menu_item = PaletteMenuItem(_('Stop'), 'activity-stop')