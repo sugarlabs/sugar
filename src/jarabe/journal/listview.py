9e494ef056f44b50ea0cfd8e4ba5a2211983cf6f
Manuel Quiñones <manuq@laptop.org>
2012-11-22 15:23:23 -0300

    Journal list: call do_get_value directly to get the buddy palette working again - SL #3888
    This is a workaround for pygobject bug
    https://bugzilla.gnome.org/show_bug.cgi?id=689277 .
    
    Signed-off-by: Manuel Quiñones <manuq@laptop.org>
    Acked-by: Simon Schampijer <simon@laptop.org>

--- src/jarabe/journal/listview.py
+++ src/jarabe/journal/listview.py
@@ -678,8 +678,13 @@ class CellRendererBuddy(CellRendererIcon):
         tree_model = self.tree_view.get_model()
         row = tree_model[self.props.palette_invoker.path]
 
-        if row[self._model_column_index] is not None:
-            nick, xo_color = row[self._model_column_index]
+        # FIXME workaround for pygobject bug, see
+        # https://bugzilla.gnome.org/show_bug.cgi?id=689277
+
+        # if row[self._model_column_index] is not None:
+        #     nick, xo_color = row[self._model_column_index]
+        if row.model.do_get_value(row.iter, self._model_column_index) is not None:
+            nick, xo_color = row.model.do_get_value(row.iter, self._model_column_index)
             return BuddyPalette((nick, xo_color.to_string()))
         else:
             return None
