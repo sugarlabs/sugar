66cfee501d9dee047658d68a860f0df02f762685
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-11 22:39:30 +0200

    Implement datastore create and update APIs

--- src/jarabe/apisocket.py
+++ src/jarabe/apisocket.py
@@ -17,38 +17,142 @@
 
 import json
 import os
+import time
 
+import dbus
 from gi.repository import GConf
 from gi.repository import GLib
 from gwebsockets.server import Server
+from gwebsockets.server import Message
+
+from sugar3 import env
 
 from jarabe.model import shell
 
 
+class StreamMonitor:
+    def __init__(self):
+        self.on_data = None
+        self.on_close = None
+
+
 class API:
-    def __init__(self, activity_id):
-        self._activity_id = activity_id
+    def __init__(self, client):
+        self._client = client
+
+        self._activity = None
+        for activity in shell.get_model():
+            if activity.get_activity_id() == client.activity_id:
+                self._activity = activity
 
 
 class ActivityAPI(API):
-    def get_xo_color(self):
-        client = GConf.Client.get_default()
-        color_string = client.get_string('/desktop/sugar/user/color')
-        return color_string.split(",")
+    def get_xo_color(self, request):
+        gconf_client = GConf.Client.get_default()
+        color_string = gconf_client.get_string('/desktop/sugar/user/color')
 
-    def close(self):
-        for activity in shell.get_model():
-            if activity.get_activity_id() == self._activity_id:
-                activity.get_window().close(GLib.get_current_time())
+        self._client.send_result(request, color_string.split(","))
+
+    def close(self, request):
+        self._activity.get_window().close(GLib.get_current_time())
+
+        self._client.send_result(request, None)
+
+
+class DatastoreAPI(API):
+    def __init__(self, client):
+        API.__init__(self, client)
+
+        bus = dbus.SessionBus()
+        bus_object = bus.get_object("org.laptop.sugar.DataStore",
+                                    "/org/laptop/sugar/DataStore")
+        self._data_store = dbus.Interface(bus_object,
+                                          "org.laptop.sugar.DataStore")
+
+    def _create_file(self):
+        activity_root = env.get_profile_path(self._activity.get_type())
+        instance_path = os.path.join(activity_root, "instance")
+
+        file_path = os.path.join(instance_path, "%i" % time.time())
+        file_object = open(file_path, "w")
+
+        return file_path, file_object
+
+    def update(self, request):
+        def reply_handler():
+            self._client.send_result(request, None)
+
+        def error_handler(error):
+            self._client.send_error(request, error)
+
+        def on_data(data):
+            file_object.write(data)
+
+        def on_close():
+            file_object.close()
+            self._data_store.update(uid, metadata, file_path, True,
+                                    reply_handler=reply_handler,
+                                    error_handler=error_handler)
+
+        uid, metadata, stream_id = request["params"]
+
+        file_path, file_object = self._create_file()
+
+        stream_monitor = self._client.stream_monitors[stream_id]
+        stream_monitor.on_data = on_data
+        stream_monitor.on_close = on_close
+
+    def create(self, request):
+        def reply_handler(object_id):
+            self._client.send_result(request, object_id)
+
+        def error_handler(error):
+            self._client.send_error(request, error)
+
+        def on_data(data):
+           file_object.write(data)
+
+        def on_close():
+            file_object.close()
+            self._data_store.create(metadata, file_path, True,
+                                    reply_handler=reply_handler,
+                                    error_handler=error_handler)
+
+        metadata, stream_id = request["params"]
+
+        file_path, file_object = self._create_file()
+
+        stream_monitor = self._client.stream_monitors[stream_id]
+        stream_monitor.on_data = on_data
+        stream_monitor.on_close = on_close
 
 
 class APIClient:
-    def __init__(self):
+    def __init__(self, session):
+        self._session = session
+
         self.activity_id = None
+        self.stream_monitors = {}
+
+    def send_result(self, request, result):
+        response = {"result": result,
+                    "error": None,
+                    "id": request["id"]}
+
+        self._session.send_message(json.dumps(response))
+
+    def send_error(self, request, error):
+        response = {"result": None,
+                    "error": error,
+                    "id": request["id"]}
+
+        self._session.send_message(json.dumps(response))
 
 
 class APIServer:
     def __init__(self):
+        self._stream_monitors = {}
+
         self._server = Server()
         self._server.connect("session-started", self._session_started_cb)
         self._port = self._server.start()
@@ -56,38 +160,60 @@ class APIServer:
 
         self._apis = {}
         self._apis["activity"] = ActivityAPI
+        self._apis["datastore"] = DatastoreAPI
 
     def setup_environment(self):
         os.environ["SUGAR_APISOCKET_PORT"] = str(self._port)
         os.environ["SUGAR_APISOCKET_KEY"] = self._key
 
+    def _open_stream(self, client, request):
+        for stream_id in xrange(0, 255):
+            if stream_id not in client.stream_monitors:
+                client.stream_monitors[stream_id] = StreamMonitor()
+                break
+
+        client.send_result(request, stream_id)
+
+    def _close_stream(self, client, request):
+        stream_id = request["params"][0]
+        stream_monitor = client.stream_monitors[stream_id]
+        if stream_monitor.on_close:
+            stream_monitor.on_close()
+
+        del client.stream_monitors[stream_id]
+
+        client.send_result(request, None)
+
     def _session_started_cb(self, server, session):
         session.connect("message-received",
-                        self._message_received_cb, APIClient())
+                        self._message_received_cb, APIClient(session))
 
-    def _message_received_cb(self, session, message, api_client):
+    def _message_received_cb(self, session, message, client):
+        if message.message_type == Message.TYPE_BINARY:
+            stream_id = ord(message.data[0])
+            stream_monitor = client.stream_monitors[stream_id]
+            stream_monitor.on_data(message.data[1:])
+            return
+
         request = json.loads(message.data)
 
         if request["method"] == "authenticate":
             params = request["params"]
             if self._key == params[1]:
-                api_client.activity_id = params[0]
+                client.activity_id = params[0]
                 return
 
-        activity_id = api_client.activity_id
+        activity_id = client.activity_id
         if activity_id is None:
             return
 
-        api_name, method_name = request["method"].split(".")
-        method = getattr(self._apis[api_name](activity_id), method_name)
-
-        result = method(*request["params"])
-
-        response = {"result": result,
-                    "error": None,
-                    "id": request["id"]}
-
-        session.send_message(json.dumps(response))
+        if request["method"] == "open_stream":
+            self._open_stream(client, request)
+        elif request["method"] == "close_stream":
+            self._close_stream(client, request)
+        else:
+            api_name, method_name = request["method"].split(".")
+            getattr(self._apis[api_name](client), method_name)(request)
 
 
 def start():
