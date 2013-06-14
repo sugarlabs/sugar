3d006e902cedc67e7792e6b3bc4d77faf2e86a6b
Daniel Narvaez <dwnarvaez@gmail.com>
2013-06-14 23:11:47 +0200

    Revert "Add menu items to journal palettes for webservices"
    This reverts commit 6d226a77efd25284f433dc4f39d744560cc1174d.

--- src/jarabe/journal/palettes.py
+++ src/jarabe/journal/palettes.py
@@ -39,7 +39,6 @@ from jarabe.model import mimeregistry
 from jarabe.journal import misc
 from jarabe.journal import model
 from jarabe.journal import journalwindow
-from jarabe.web import accountsmanager
 
 
 class ObjectPalette(Palette):
@@ -250,10 +249,6 @@ class CopyMenu(Gtk.Menu):
             volume_menu.connect('volume-error', self.__volume_error_cb)
             self.append(volume_menu)
             volume_menu.show()
-
-        for account in accountsmanager.get_configured_accounts():
-            self.append(
-                account.get_shared_journal_entry().get_share_menu(metadata))
 
     def __volume_error_cb(self, menu_item, message, severity):
         self.emit('volume-error', message, severity)
