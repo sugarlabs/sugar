6356ec6003576a890fc409faf85c954e152aa73b
Walter Bender <walter@sugarlabs.org>
2013-06-18 11:09:39 -0400

    Add extensions support for webservice/webaccount
    This is another patch in a series of patches for interacting with
    online accounts from Sugar. The relevant feature request is [1].
    
    This patch introduces two new sections in sugar extensions:
    
    (1) webservice is stub for installing webservices; each web service is
    installed in a subdirectory of this directory.
    
    (2) cpsection/webaccount is a new control panel section for managing
    web service accounts. Account management services are loaded into
    individual subdirectories of cpsection/webaccount/services.
    
    Note that this same directory structure can be set up in
    ~/.sugar/extensions for web services installed by the end
    user. pkgutil is used to resolve namespace issues.
    
    [1] http://wiki.sugarlabs.org/go/Features/Web_services
    
    A unit test for the new cpsection will be included in a separate patch.

---
+++ extensions/cpsection/webaccount/view.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2013, Walter Bender - Raul Gutierrez Segales
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+import os
+import logging
+from importlib import import_module
+
+from gi.repository import Gtk
+from gettext import gettext as _
+
+from jarabe.webservice.accountsmanager import find_module, get_webaccount_paths
+from jarabe.controlpanel.sectionview import SectionView
+
+from sugar3.graphics.icon import CanvasIcon
+from sugar3.graphics import style
+
+
+class WebServicesConfig(SectionView):
+    def __init__(self, model, alerts):
+        SectionView.__init__(self)
+
+        self._model = model
+        self.restart_alerts = alerts
+
+        services = _get_services()
+        if len(services) == 0:
+            label = Gtk.Label(_('No web services are installed.\n\
+Please visit http://wiki.sugarlabs.org/go/WebServices for more details.'))
+            label.show()
+            self.add(label)
+            return
+
+        vbox = Gtk.VBox()
+        hbox = Gtk.HBox(style.DEFAULT_SPACING)
+
+        self._service_config_box = Gtk.VBox()
+
+        for service in services:
+            icon = CanvasIcon(icon_name=service.get_icon_name())
+            icon.connect('button_press_event',
+                         service.config_service_cb,
+                         self._service_config_box)
+            icon.show()
+            hbox.pack_start(icon, False, False, 0)
+
+        hbox.show()
+        vbox.pack_start(hbox, False, False, 0)
+
+        scrolled = Gtk.ScrolledWindow()
+        vbox.pack_start(scrolled, True, True, 0)
+
+        self.add(vbox)
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled.show()
+
+        workspace = Gtk.VBox()
+        scrolled.add_with_viewport(workspace)
+        workspace.show()
+
+        workspace.add(self._service_config_box)
+        workspace.show_all()
+        vbox.show()
+
+    def undo(self):
+        pass
+
+
+def _get_services():
+    service_paths = []
+    for path in get_webaccount_paths():
+        service_paths.append(os.path.join(path, 'services'))
+
+    services = []
+    for service_path in service_paths:
+        if not os.path.exists(service_path):
+            continue
+
+        folders = os.listdir(service_path)
+        for folder in folders:
+            if not os.path.isdir(os.path.join(service_path, folder)):
+                continue
+
+            if not os.path.exists(os.path.join(
+                    service_path, folder, 'service.py')):
+                continue
+
+            mod = load_module(os.path.join(service_path, folder),
+                                         'service')
+            if hasattr(mod, 'get_service'):
+                services.append(mod.get_service())
+
+    return services
+
+
+def load_module(path, module):
+    mod = None
+
+    try:
+        mod = import_module(find_module(path, module), [module])
+    except ImportError, e:
+        logging.debug('ImportError: %s' % (e))
+
+    return mod
