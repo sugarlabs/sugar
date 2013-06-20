4f6dc29c7833d6eca5e2ee40398b85b4d43d0a75
Walter Bender <walter@sugarlabs.org>
2013-06-20 06:56:01 -0400

    Add webaccount/webservice extension support
    This is another patch in a series of patches for interacting with
    online accounts from Sugar.  The relevant feature request is [1].
    
    This patch introduces two new sections in sugar extensions:
    
    (1) webservice is stub for installing webservices; each web service is
    installed in a subdirectory of this directory.
    
    (2) cpsection/webaccount is a new control panel section for managing
    web service accounts. Account management services are loaded into
    individual subdirectories of cpsection/webaccount/services.
    
    A new helper method was also added to
    jarabe/webservice/accountsmanager and the unit test as updated accordingly.
    
    Note that this same directory structure can be set up in
    env.get_profile_path()/extensions for web services installed by the
    end user.
    
    [1] http://wiki.sugarlabs.org/go/Features/Web_services
    
    A first pass at a unit test for the new webaccount cpsection is also
    included. For naming consistency, this test was called test_webaccount
    while the exsiting test_webaccount was renamed to the more appropriate
    test_webservice.

---
+++ tests/test_webservice.py
@@ -0,0 +1,115 @@
+# Copyright (C) 2013, Walter Bender
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os
+import sys
+import unittest
+
+from gi.repository import Gtk
+
+from jarabe import config
+from jarabe.webservice.account import Account
+from jarabe.webservice import accountsmanager
+
+ACCOUNT_NAME = 'mock'
+
+tests_dir = os.getcwd()
+extension_dir = os.path.join(tests_dir, 'extensions')
+web_extension_dir = os.path.join(extension_dir, 'webservice')
+
+
+class TestWebAccounts(unittest.TestCase):
+    def setUp(self):
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_NONE)
+        self.save_ext_path = config.ext_path
+        config.ext_path = extension_dir
+        sys.path.append(config.ext_path)
+
+    def test_get_description(self):
+        accounts = accountsmanager.get_all_accounts()
+        found_mock_account = False
+        for account in accounts:
+            if account.get_description() == ACCOUNT_NAME:
+                found_mock_account = True
+                break
+        self.assertTrue(found_mock_account)
+
+    def test_icon_theme(self):
+        icon_theme = Gtk.IconTheme.get_default()
+        icon_search_path = icon_theme.get_search_path()
+        icon_path = os.path.join(web_extension_dir, ACCOUNT_NAME, 'icons')
+        self.assertTrue(icon_path in icon_search_path)
+
+    def test_get_webaccount_services(self):
+        services = accountsmanager.get_webaccount_services()
+        self.assertTrue(len(services) > 0)
+
+    def test_get_all_accounts(self):
+        accounts = accountsmanager.get_all_accounts()
+        self.assertTrue(len(accounts) > 0)
+
+    def test_get_account(self):
+        account = accountsmanager.get_account('mock')
+        self.assertIsNotNone(account)
+
+    def test_get_service(self):
+        account = accountsmanager.get_service('mock')
+        self.assertIsNotNone(account)
+
+    def test_get_configured_accounts(self):
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_VALID)
+        accounts = accountsmanager.get_configured_accounts()
+        count = len(accounts)
+        self.assertTrue(count > 0)
+
+        self.assertTrue(accountsmanager.has_configured_accounts())
+
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_NONE)
+        accounts = accountsmanager.get_configured_accounts()
+        self.assertTrue(len(accounts) == count - 1)
+
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_EXPIRED)
+        accounts = accountsmanager.get_configured_accounts()
+        self.assertTrue(len(accounts) == count)
+
+    def test_get_active_accounts(self):
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_VALID)
+        accounts = accountsmanager.get_active_accounts()
+        count = len(accounts)
+        self.assertTrue(count > 0)
+
+        os.environ["MOCK_ACCOUNT_STATE"] = str(Account.STATE_EXPIRED)
+        accounts = accountsmanager.get_active_accounts()
+        self.assertTrue(len(accounts) == count - 1)
+
+    def test_share_menu(self):
+        accounts = accountsmanager.get_all_accounts()
+        for account in accounts:
+            shared_journal_entry = account.get_shared_journal_entry()
+            share_menu = shared_journal_entry.get_share_menu(
+                {'account': 'mock'})
+            self.assertIsNotNone(share_menu)
+
+    def test_refresh_menu(self):
+        accounts = accountsmanager.get_all_accounts()
+        for account in accounts:
+            shared_journal_entry = account.get_shared_journal_entry()
+            refresh_menu = shared_journal_entry.get_refresh_menu()
+            self.assertIsNotNone(refresh_menu)
+
+    def tearDown(self):
+        sys.path.remove(config.ext_path)
+        config.ext_path = self.save_ext_path
