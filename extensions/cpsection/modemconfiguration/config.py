048c068ab6babb0c21bde02090f898a03a953c1e
Miguel Gonzalez <migonzalvar@gmail.com>
2013-07-02 22:13:21 +0200

    Add config.py file with GConf keys and file paths

---
+++ extensions/cpsection/modemconfiguration/config.py
@@ -0,0 +1,7 @@
+PROVIDERS_PATH = "/usr/share/mobile-broadband-provider-info/serviceproviders.xml"
+PROVIDERS_FORMAT_SUPPORTED = "2.0"
+COUNTRY_CODES_PATH = "/usr/share/zoneinfo/iso3166.tab"
+
+GCONF_SP_COUNTRY = '/desktop/sugar/network/gsm/country'
+GCONF_SP_PROVIDER = '/desktop/sugar/network/gsm/provider'
+GCONF_SP_PLAN = '/desktop/sugar/network/gsm/plan'
