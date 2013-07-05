4e46f8182c2fc7d7766cca9231110cab0e2a3912
Walter Bender <walter@sugarlabs.org>
2013-07-05 18:22:09 -0400

    use get_name(), get_description() instead of name, description in order to circumvent problem with byte-array conversion to utf-8

--- extensions/cpsection/keyboard/model.py
+++ extensions/cpsection/keyboard/model.py
@@ -17,6 +17,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+import logging
+
 from gi.repository import Xkl
 from gi.repository import GConf
 from gi.repository import SugarExt
@@ -26,13 +28,6 @@ _GROUP_NAME = 'grp'  # The XKB name for group switch options
 _LAYOUTS_KEY = '/desktop/sugar/peripherals/keyboard/layouts'
 _OPTIONS_KEY = '/desktop/sugar/peripherals/keyboard/options'
 _MODEL_KEY = '/desktop/sugar/peripherals/keyboard/model'
-
-
-def _item_str(s):
-    '''Convert a zero-terminated byte array to a proper str'''
-
-    i = s.find(b'\x00')
-    return s[:i].decode("utf-8")
 
 
 class KeyboardManager(object):
@@ -46,16 +41,19 @@ class KeyboardManager(object):
         self._gconf_client = GConf.Client.get_default()
 
     def _populate_one(self, config_registry, item, store):
-        store.append([_item_str(item.description), _item_str(item.name)])
+        store.append([item.get_description().decode("utf-8"),
+                      item.get_name().decode("utf-8")])
 
     def _populate_two(self, config_registry, item, subitem, store):
-        layout = _item_str(item.name)
+        layout = item.get_name().decode("utf-8")
         if subitem:
-            description = '%s, %s' % (_item_str(subitem.description),
-                                      _item_str(item.description))
-            variant = _item_str(subitem.name)
+            description = '%s, %s' % (
+                subitem.get_description().decode("utf-8"),
+                item.get_description().decode("utf-8"))
+            variant = subitem.get_name().decode("utf-8")
         else:
-            description = 'Default layout, %s' % _item_str(item.description)
+            description = 'Default layout, %s' % \
+                item.get_description().decode("utf-8")
             variant = ''
         store.append([description, ('%s(%s)' % (layout, variant))])
 
