61f760ea19866f6db7ed3fa396595fa377665788
Miguel Gonzalez <migonzalvar@activitycentral.com>
2013-07-15 13:13:51 +0200

    Use \ to continue long line

--- extensions/cpsection/modemconfiguration/model.py
+++ extensions/cpsection/modemconfiguration/model.py
@@ -29,8 +29,9 @@ from gi.repository import GConf
 from jarabe.model import network
 
 
-PROVIDERS_PATH = ("/usr/share/mobile-broadband-provider-info/"
-                  "serviceproviders.xml")
+PROVIDERS_PATH = "/usr/share/mobile-broadband-provider-info/"\
+                 "serviceproviders.xml"
+
 PROVIDERS_FORMAT_SUPPORTED = "2.0"
 COUNTRY_CODES_PATH = "/usr/share/zoneinfo/iso3166.tab"
 
