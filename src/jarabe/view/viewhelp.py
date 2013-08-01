3775bc25784bc56cfafb430fe120f67ff039d5d2
Kalpa Welivitigoda <callkalpa@gmail.com>
2013-08-01 21:50:27 +0530

    fixed some spaces and tabs issues

--- src/jarabe/view/viewhelp.py
+++ src/jarabe/view/viewhelp.py
@@ -22,6 +22,7 @@ from gi.repository import Gtk
 from gi.repository import GObject
 from gi.repository import Gdk
 from gi.repository import WebKit
+from gi.repository import GdkX11
 
 from sugar3 import env
 from sugar3.graphics import style
@@ -53,7 +54,7 @@ def setup_view_help(activity):
 
     # get the help file name for the activity
     try:
-	help_content_link = os.path.join(activity_path, 'Help.activity/helplink.json')
+        help_content_link = os.path.join(activity_path, 'Help.activity/helplink.json')
         json_file = open(help_content_link)
         links = json.load(json_file)
 
@@ -61,23 +62,26 @@ def setup_view_help(activity):
         if activity_name in links.keys():
             viewhelp = ViewHelp(activity_name, links[activity_name], window_xid)
             viewhelp.show()
+        else:
+            _logger.error('Help content is not available for the activity')
     except Exception:
-    	_logger.error('helplink.json file was not found or the json was malformed')
+        _logger.error('helplink.json file was not found or the json was malformed')
 
 
 class ViewHelp(Gtk.Window):
     parent_window_xid = None
-    def __init__(self, activity_name, help_file, window_xid):
-        parent_window_xid = window_xid
 
-    	# initiate Gtk Window
+    def __init__(self, activity_name, help_file, window_xid):
+        self.parent_window_xid = window_xid
+
+        # initiate Gtk Window
         Gtk.Window.__init__(self)
         box = Gtk.Box()
         box.set_orientation(Gtk.Orientation.VERTICAL)
         self.add(box)
         box.show()
-        
-	self.set_decorated(False)
+
+        self.set_decorated(False)
         self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
         self.set_border_width(style.LINE_WIDTH)
         self.set_has_resize_grip(False)
@@ -86,7 +90,7 @@ class ViewHelp(Gtk.Window):
         height = Gdk.Screen.height() - style.GRID_CELL_SIZE * 2
         self.set_size_request(width, height)
 
-	self.connect('realize', self.realize_cb)
+        self.connect('realize', self.realize_cb)
 
         # implement the tool bar
         toolbar = Toolbar(activity_name)
@@ -106,11 +110,11 @@ class ViewHelp(Gtk.Window):
 
         webview.show()
 
-	# get the current language and display load the relavent html file
-        
-	
-	
-	language = self.get_current_language()
+        # get the current language and display load the relavent html file
+
+
+
+        language = self.get_current_language()
         locale_dir = self.get_locale_directory(language)
 
 
@@ -123,13 +127,13 @@ class ViewHelp(Gtk.Window):
         self.destroy()
 
     def realize_cb(self, widget):
-    	# focus the help viewer and prompt it over the activity window
+        # focus the help viewer and prompt it over the activity window
         self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
-	window = self.get_window()
-	window.set_accept_focus(True)
-	display = Gdk.Display.get_default()
-	parent = GdkX11.X11Window.foreign_new_for_display(display, self.parent_window_xid)
-	window.set_transient_for(parent)
+        window = self.get_window()
+        window.set_accept_focus(True)
+        display = Gdk.Display.get_default()
+        parent = GdkX11.X11Window.foreign_new_for_display(display, self.parent_window_xid)
+        window.set_transient_for(parent)
 
     def get_current_language(self):
         language_file_path = os.path.join(expanduser('~'), '.i18n')
