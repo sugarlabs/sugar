ea9ecf256e6ba1403f978125eb742b03d95adc10
Manuel Quiñones <manuq@laptop.org>
2013-08-22 13:14:21 -0300

    View Source: don't crash if toolkit path wasn't found - SL #4605
    Log an error instead.
    
    Toolbar class was considering a value of None for the path, but the
    ViewSource class wasn't.

--- src/jarabe/view/viewsource.py
+++ src/jarabe/view/viewsource.py
@@ -105,6 +105,9 @@ def setup_view_source(activity):
                 sugar_toolkit_path = os.path.join(path, 'sugar')
                 break
 
+    if sugar_toolkit_path is None:
+        _logger.error("Path to toolkit not found.")
+
     view_source = ViewSource(window_xid, bundle_path, document_path,
                              sugar_toolkit_path, activity.get_title())
     map_activity_to_window[window_xid] = view_source
@@ -176,13 +179,19 @@ class ViewSource(Gtk.Window):
         tree_panes.add1(self._bundle_source_viewer)
         self._bundle_source_viewer.show()
 
-        file_name = 'env.py'
-        self._selected_sugar_file = os.path.join(sugar_toolkit_path, file_name)
-        self._sugar_source_viewer = FileViewer(sugar_toolkit_path, file_name)
-        self._sugar_source_viewer.connect('file-selected',
-                                          self.__file_selected_cb)
-        tree_panes.add2(self._sugar_source_viewer)
-        self._sugar_source_viewer.hide()
+        self._selected_sugar_file = None
+        self._sugar_source_viewer = None
+
+        if sugar_toolkit_path is not None:
+            file_name = 'env.py'
+            self._selected_sugar_file = os.path.join(sugar_toolkit_path,
+                                                     file_name)
+            self._sugar_source_viewer = FileViewer(sugar_toolkit_path,
+                                                   file_name)
+            self._sugar_source_viewer.connect('file-selected',
+                                              self.__file_selected_cb)
+            tree_panes.add2(self._sugar_source_viewer)
+            self._sugar_source_viewer.hide()
 
         pane.add1(tree_panes)
 
@@ -222,7 +231,10 @@ class ViewSource(Gtk.Window):
             _logger.debug('_select_source called with file: %r', path)
             self._source_display.file_path = path
             self._bundle_source_viewer.hide()
-            self._sugar_source_viewer.hide()
+
+            if self._sugar_source_viewer is not None:
+                self._sugar_source_viewer.hide()
+
         elif path == self._sugar_toolkit_path:
             _logger.debug('_select_source called with sugar toolkit path: %r',
                           path)
@@ -235,7 +247,9 @@ class ViewSource(Gtk.Window):
             self._bundle_source_viewer.set_path(path)
             self._source_display.file_path = self._selected_bundle_file
             self._bundle_source_viewer.show()
-            self._sugar_source_viewer.hide()
+
+            if self._sugar_source_viewer is not None:
+                self._sugar_source_viewer.hide()
 
     def __destroy_cb(self, window, document_path):
         del map_activity_to_window[self._parent_window_xid]
