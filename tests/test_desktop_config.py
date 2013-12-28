23ca5d35b176f6ee9131e78ebad10ef491f90148
Emil Dudev <emildudev@gmail.com>
2013-12-28 20:35:13 +0200

    GConf to GSetitngs port, tier 2 Merges several keys for the homeviews into a list of dictionaries

--- tests/test_desktop_config.py
+++ tests/test_desktop_config.py
@@ -17,15 +17,19 @@
 from sugar3.test import unittest
 
 from gi.repository import Gio
+from gi.repository import GLib
 
 from jarabe.model import desktop
 
 _DESKTOP_CONF_DIR = 'org.sugarlabs.desktop'
-_VIEW_KEY = 'view-icons'
-_FAVORITE_KEY = 'favorite-icons'
+_HOMEVIEWS_KEY = 'homeviews'
 
-_VIEW_ICONS = ['view-radial']
-_MOCK_LIST = ['view-radial', 'view-random']
+_VIEW_ICONS = [{'layout': 'box-layout', 'view-icon': 'view-radial',
+               'favorite-icon': 'emblem-locked'}]
+_MOCK_LIST = [{'layout': 'box-layout', 'view-icon': 'view-radial',
+               'favorite-icon': 'emblem-locked'},
+              {'layout': 'ring-layout', 'view-icon': 'view-box',
+               'favorite-icon': 'emblem-favorite'}]
 
 
 class TestDesktopConfig(unittest.UITestCase):
@@ -34,22 +38,22 @@ class TestDesktopConfig(unittest.UITestCase):
         self.target = []
 
         settings = Gio.Settings(_DESKTOP_CONF_DIR)
-        self._save_view_icons = settings.get_strv(_VIEW_KEY)
-        self._save_favorite_icons = settings.get_strv(_FAVORITE_KEY)
+        self._save_homeviews = settings.get_value(_HOMEVIEWS_KEY)
 
         self.model = desktop.get_model()
         self.model.connect('desktop-view-icons-changed',
                            self.__desktop_view_icons_changed_cb)
 
-    def __desktop_view_icons_changed_cb(self):
+    def __desktop_view_icons_changed_cb(self, model):
         number_of_views = desktop.get_number_of_views()
         self.assertTrue(number_of_views == len(self.target))
 
         view_icons = desktop.get_view_icons()
         self.assertTrue(len(view_icons) == len(self.target))
 
+        right_view_icons = [view['view-icon'] for view in self.target]
         for i in range(len(view_icons)):
-            self.assertTrue(view_icons[i] in self.target)
+            self.assertTrue(view_icons[i] in right_view_icons)
 
         favorite_icons = desktop.get_favorite_icons()
         self.assertTrue(len(favorite_icons) >= len(self.target))
@@ -58,22 +62,17 @@ class TestDesktopConfig(unittest.UITestCase):
         self.target = _VIEW_ICONS
         with self.run_view("gtk_main"):
             settings = Gio.Settings(_DESKTOP_CONF_DIR)
-            settings.set_strv(_VIEW_KEY, [])
+            variant = GLib.Variant('aa{ss}', self.target)
+            settings.set_value(_HOMEVIEWS_KEY, variant)
 
     def test_set_views(self):
         self.target = _MOCK_LIST
         with self.run_view("gtk_main"):
             settings = Gio.Settings(_DESKTOP_CONF_DIR)
-            settings.set_strv(_VIEW_KEY, _MOCK_LIST)
+            variant = GLib.Variant('aa{ss}', self.target)
+            settings.set_value(_HOMEVIEWS_KEY, variant)
 
     def tearDown(self):
+        self.target = self._save_homeviews.unpack()
         settings = Gio.Settings(_DESKTOP_CONF_DIR)
-        if self._save_view_icons is None:
-            settings.set_strv(_VIEW_KEY, [])
-        else:
-            settings.set_strv(_VIEW_KEY, self._save_view_icons)
-
-        if self._save_favorite_icons is None:
-            settings.set_strv(_FAVORITE_KEY, [])
-        else:
-            settings.set_strv(_FAVORITE_KEY, self._save_favorite_icons)
+        settings.set_value(_HOMEVIEWS_KEY, self._save_homeviews)
