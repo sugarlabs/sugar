6043dac422b01327bd007f1ca162ff293903dd52
Gonzalo Odiard <godiard@gmail.com>
2014-03-12 12:49:47 -0300

    Updater: show icons for new activities - Fixes #4657
    Signed-off-by: Gonzalo Odiard <godiard@sugarlabs.org>

--- src/jarabe/model/update/__init__.py
+++ src/jarabe/model/update/__init__.py
@@ -17,9 +17,11 @@
 
 
 class BundleUpdate(object):
-    def __init__(self, bundle_id, name, version, link, size):
+    def __init__(self, bundle_id, name, version, link, size,
+                 icon_file_name=None):
         self.bundle_id = bundle_id
         self.name = name
         self.version = version
         self.link = link
         self.size = size
+        self.icon_file_name = icon_file_name
