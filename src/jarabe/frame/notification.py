416ddbf7da581d304b4810b3862ed4b1e11559f4
Sam Parkinson <sam.parkinson3@gmail.com>
2014-03-24 18:20:05 +1100

    Display badge in corner notification icon
    Replace PulsingIcon by NotificationPulsingIcon in
    NotificatioIcon class.
    
    This allows the corner notification icon to display
    the badges with the show_badge convention.

--- src/jarabe/frame/notification.py
+++ src/jarabe/frame/notification.py
@@ -196,8 +196,9 @@ class NotificationButton(ToolButton):
 
 class NotificationPulsingIcon(PulsingIcon):
 
+    position = GObject.property(type=int, default=22)
+
     SIZE = 19
-    POSITION = 22
 
     def __init__(self, filename=None, name=None, colors=None):
         PulsingIcon.__init__(self)
@@ -229,7 +230,9 @@ class NotificationPulsingIcon(PulsingIcon):
     def do_draw(self, cr):
         PulsingIcon.do_draw(self, cr)
         if self._badge:
-            cr.set_source_surface(self._badge, self.POSITION, self.POSITION)
+            cr.set_source_surface(self._badge,
+                                  self.props.position,
+                                  self.props.position)
             cr.paint()
 
 
@@ -245,7 +248,10 @@ class NotificationIcon(Gtk.EventBox):
     _PULSE_TIMEOUT = 3
 
     def __init__(self, **kwargs):
-        self._icon = PulsingIcon(pixel_size=style.STANDARD_ICON_SIZE)
+        self._icon = NotificationPulsingIcon()
+        self._icon.props.pixel_size = style.STANDARD_ICON_SIZE
+        self._icon.props.position = 28
+
         Gtk.EventBox.__init__(self, **kwargs)
         self.props.visible_window = False
 
@@ -291,6 +297,12 @@ class NotificationIcon(Gtk.EventBox):
         return self._icon.palette
 
     palette = property(_get_palette, _set_palette)
+
+    def show_badge(self):
+        self._icon.show_badge()
+
+    def hide_badge(self):
+        self._icon.hide_badge()
 
 
 class NotificationWindow(Gtk.Window):
