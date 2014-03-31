c94e6f7d35e5f36a7dcd20cb28f830acf40bcf58
Walter Bender <walter@sugarlabs.org>
2014-03-31 17:24:20 -0400

    Add control for microphone level setting This patch adds a microphone level and mute control to the speaker device on the Sugar Frame. It makes use of an extension to the sound model by Emil Dudev and a new icon, media-audio-input.svg in icons/scaleable/devices. (The extension to the sound model also requires a change in sugar-toolkit-gtk3 sugar3/acme-volume-alsa.c and sugar3/acme-volume-alsa.h).
    See [1] and [2] for the Design Team discussion.
    
    [1] http://wiki.sugarlabs.org/go/File:MicLevels.png
    [2] http://lists.sugarlabs.org/archive/sugar-devel/2014-March/047640.html
    
    Try 2: Some code consolidation by Martin and Gonzalo; rename
    speaker.py to audio.py
    
    Try 3: Remove unneed notification handlers that were interferring with
    GNOME/Sugar interactions. (Note that ASLAMIXER and Sugar interoperate
    properly with this patch.)
    
    Try 4: Martin added a class to the sound-capture code in model.py; UI
    logic clean-up.
    
    Try 5: Consolidated patch; includes patch by Gonzalo to fix grid padding
    
    Fixes SL #800

--- src/jarabe/model/sound.py
+++ src/jarabe/model/sound.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2006-2008 Red Hat, Inc.
+# Copyright (C) 2014 Emil Dudev
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,41 +21,64 @@ from gi.repository import SugarExt
 from sugar3 import dispatch
 
 
-VOLUME_STEP = 10
-
-muted_changed = dispatch.Signal()
-volume_changed = dispatch.Signal()
-
-_volume = SugarExt.VolumeAlsa()
+_PLAYBACK = 0
+_CAPTURE = 1
 
 
-def get_muted():
-    return _volume.get_mute()
+class PlaybackSound(object):
+    _volume = SugarExt.VolumeAlsa.new(_PLAYBACK)
+
+    muted_changed = dispatch.Signal()
+    volume_changed = dispatch.Signal()
+
+    VOLUME_STEP = 10
+
+    def get_muted(self):
+        return self._volume.get_mute()
+
+    def get_volume(self):
+        return self._volume.get_volume()
+
+    def set_volume(self, new_volume):
+        self._volume.set_volume(new_volume)
+        self.volume_changed.send(None)
+        self.save()
+
+    def set_muted(self, new_state):
+        self._volume.set_mute(new_state)
+        self.muted_changed.send(None)
+        self.save()
+
+    def save(self):
+        settings = Gio.Settings('org.sugarlabs.sound')
+        settings.set_int('volume', self.get_volume())
+
+    def restore(self):
+        settings = Gio.Settings('org.sugarlabs.sound')
+        self.set_volume(settings.get_int('volume'))
 
 
-def get_volume():
-    return _volume.get_volume()
+class CaptureSound(object):
+    _volume = SugarExt.VolumeAlsa.new(_CAPTURE)
+
+    muted_changed = dispatch.Signal()
+    volume_changed = dispatch.Signal()
+
+    def get_muted(self):
+        return self._volume.get_mute()
+
+    def get_volume(self):
+        return self._volume.get_volume()
+
+    def set_volume(self, new_volume):
+        self._volume.set_volume(new_volume)
+
+        self.volume_changed.send(None)
+
+    def set_muted(self, new_state):
+        self._volume.set_mute(new_state)
+        self.muted_changed.send(None)
 
 
-def set_volume(new_volume):
-    _volume.set_volume(new_volume)
-
-    volume_changed.send(None)
-    save()
-
-
-def set_muted(new_state):
-    _volume.set_mute(new_state)
-
-    muted_changed.send(None)
-    save()
-
-
-def save():
-    settings = Gio.Settings('org.sugarlabs.sound')
-    settings.set_int('volume', get_volume())
-
-
-def restore():
-    settings = Gio.Settings('org.sugarlabs.sound')
-    set_volume(settings.get_int('volume'))
+sound = PlaybackSound()
+capture_sound = CaptureSound()
