a84d6dd760045e6281780c8d869718ddef9898e8
Gonzalo Odiard <godiard@gmail.com>
2015-07-21 15:59:11 -0300

    More robust read/write of file .i18n
    We have received a few reports of broken ~/.i18n files
    Try to make the code more robust, using "with" to open the files,
    and remove redundancy in the code.
    Only write the file when set the languages, and flush to
    try avoid have a broken file.
    When read the languages, get the values from the enviroment variables
    set on bin/sugar. If the file ~/.i18n exists, was already read there.

--- extensions/cpsection/language/model.py
+++ extensions/cpsection/language/model.py
@@ -25,7 +25,6 @@ import locale
 from gettext import gettext as _
 import subprocess
 
-
 _default_lang = '%s.%s' % locale.getdefaultlocale()
 _standard_msg = _('Could not access ~/.i18n. Create standard settings.')
 
@@ -81,49 +80,33 @@ def _initialize():
 
 
 def _write_i18n(lang_env, language_env):
-    path = os.path.join(os.environ.get('HOME'), '.i18n')
-    if not os.access(path, os.W_OK):
-        print _standard_msg
-        fd = open(path, 'w')
-        fd.write('LANG="%s"\n' % _default_lang)
-        fd.write('LANGUAGE="%s"\n' % _default_lang)
-        fd.close()
-    else:
-        fd = open(path, 'w')
+    path = os.path.join(os.environ.get('HOME', ''), '.i18n')
+    with open(path, 'w') as fd:
         fd.write('LANG="%s"\n' % lang_env)
         fd.write('LANGUAGE="%s"\n' % language_env)
+        fd.flush()
+        # be sure all is flushed
+        os.fsync(fd)
         fd.close()
 
 
 def get_languages():
-    path = os.path.join(os.environ.get('HOME', ''), '.i18n')
-    if not os.access(path, os.R_OK):
-        print _standard_msg
-        fd = open(path, 'w')
-        fd.write('LANG="%s"\n' % _default_lang)
-        fd.write('LANGUAGE="%s"\n' % _default_lang)
-        fd.close()
-        return [_default_lang]
-
-    fd = open(path, 'r')
-    lines = fd.readlines()
-    fd.close()
-
+    # read the env variables set in bin/sugar
     langlist = None
     lang = _default_lang
 
-    for line in lines:
-        if line.startswith('LANGUAGE='):
-            lang = line[9:].replace('"', '')
-            lang = lang.strip()
-            if lang.endswith('UTF-8'):
-                lang = lang.replace('UTF-8', 'utf8')
-            langlist = lang.split(':')
-        elif line.startswith('LANG='):
-            lang = line[5:].replace('"', '')
-            lang = lang.strip()
-            if lang.endswith('UTF-8'):
-                lang = lang.replace('UTF-8', 'utf8')
+    if 'LANGUAGE' in os.environ:
+        lang = os.environ['LANGUAGE']
+        lang = lang.strip()
+        if lang.endswith('UTF-8'):
+            lang = lang.replace('UTF-8', 'utf8')
+        langlist = lang.split(':')
+
+    if 'LANG' in os.environ:
+        lang = os.environ['LANG']
+        lang = lang.strip()
+        if lang.endswith('UTF-8'):
+            lang = lang.replace('UTF-8', 'utf8')
 
     # There might be cases where .i18n may not contain a LANGUAGE field
     if langlist is None:
