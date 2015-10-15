e1b97c04c2375875a97bebf4211d530aae74a191
James Cameron <quozl@laptop.org>
2015-10-15 13:32:19 +1100

    check before remove on control panel section end
    A critical message appears in shell.log when a control panel section is
    removed from screen:
    
    (main.py:6713): Gtk-CRITICAL **: gtk_container_remove: assertion 'gtk_widget_get_parent (widget) == GTK_WIDGET (container) || GTK_IS_ASSISTANT (container)' failed
    
    There is no other effect of the message.
    
    Fix the warning by checking the object is in the container before trying
    to remove it.
    
    Since None can't be a child of the container, the check for None is
    redundant, so is removed.

--- src/jarabe/controlpanel/gui.py
+++ src/jarabe/controlpanel/gui.py
@@ -110,7 +110,7 @@ class ControlPanel(Gtk.Window):
             self._setup_options()
 
     def _set_canvas(self, canvas):
-        if self._canvas:
+        if self._canvas in self._main_view:
             self._main_view.remove(self._canvas)
         if canvas:
             self._main_view.add(canvas)
