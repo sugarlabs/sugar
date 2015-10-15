a1230b0d82f7568a0f46b503f7eebedde1154dc5
James Cameron <quozl@laptop.org>
2015-10-15 18:05:18 +1100

    add quit failure alert
    Add a quit failure alert to the menu and the control panel.  After 30
    seconds with no click, continue with the quit.
    
    Why?
    
    The buddy menu has options for shutdown, restart or logout.  The control
    panel may also restart.  These make Sugar quit.  Session manager sends a
    save-yourself message to running activities, and waits for the
    activities to respond.
    
    When an activity does not respond, Sugar does not quit.  No reason is
    given.
    
    Adding the alert will allow user to cancel the shutdown, cancel the
    control panel section change, and work the problem, or lose unsaved
    work.
    
    Caution: requires a patch to
    
        sugar-toolkit-gtk3:src/sugar3/graphics/alert.py
    
    otherwise a forced shutdown will occur 30 seconds after the alert is
    shown, even if Cancel is clicked.

--- src/jarabe/controlpanel/gui.py
+++ src/jarabe/controlpanel/gui.py
@@ -24,7 +24,7 @@ from gi.repository import Gdk
 
 from sugar3.graphics.icon import Icon
 from sugar3.graphics import style
-from sugar3.graphics.alert import Alert
+from sugar3.graphics.alert import Alert, TimeoutAlert
 
 from jarabe.model.session import get_session_manager
 from jarabe.controlpanel.toolbar import MainToolbar
@@ -399,8 +399,31 @@ class ControlPanel(Gtk.Window):
                 self._section_view.restart_alerts
             self._show_main_view()
         elif response_id is Gtk.ResponseType.APPLY:
-            session_manager = get_session_manager()
-            session_manager.logout()
+            self.busy()
+            self._section_toolbar.accept_button.set_sensitive(False)
+            self._section_toolbar.cancel_button.set_sensitive(False)
+            get_session_manager().logout()
+            GObject.timeout_add_seconds(4, self.__quit_timeout_cb)
+
+    def __quit_timeout_cb(self):
+        self.unbusy()
+        alert = TimeoutAlert(30)
+        alert.props.title = _('An activity is not responding.')
+        alert.props.msg = _('You may lose unsaved work if you continue.')
+        alert.connect('response', self.__quit_accept_cb)
+
+        self.add_alert(alert)
+        alert.show()
+
+    def __quit_accept_cb(self, alert, response_id):
+        self.remove_alert(alert)
+        if response_id is Gtk.ResponseType.CANCEL:
+            get_session_manager().cancel_shutdown()
+            self._section_toolbar.accept_button.set_sensitive(True)
+            self._section_toolbar.cancel_button.set_sensitive(True)
+        else:
+            self.busy()
+            get_session_manager().shutdown_completed()
 
     def __select_option_cb(self, button, event, option):
         self.show_section_view(option)
