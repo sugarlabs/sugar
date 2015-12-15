sugarlabs/sugar

commit e43d177d5ecc820f40308f930d1c65c3de109895

Add accelerators for control panel open

src/jarabe/view/keyhandler.py
@@ -31,6 +31,7 @@ from jarabe.view.tabbinghandler import TabbingHandler
 from jarabe.model.shell import ShellModel
 from jarabe import config
 from jarabe.journal import journalactivity
+from jarabe.controlpanel.gui import ControlPanel
 
 
 _VOLUME_STEP = sound.VOLUME_STEP
@@ -59,6 +60,7 @@ _actions_table = {
     'XF86WebCam': 'open_search',
     '<alt><shift>f': 'frame',
     'XF86Search': 'open_search',
+    '<alt><shift>m': 'open_controlpanel',
     '<alt><shift>o': 'open_search',
     '<alt><shift>q': 'logout',
     '<alt><shift>d': 'dump_ui_tree'
@@ -166,6 +168,12 @@ class KeyHandler(object):
 
     def handle_open_search(self, event_time):
         journalactivity.get_journal().show_journal()
+
+    def handle_open_controlpanel(self, event_time):
+        if shell.get_model().has_modal():
+            return
+        panel = ControlPanel()
+        panel.show()
 
     def handle_dump_ui_tree(self, event_time):
         print uitree.get_root().dump()

commit 16b06c47c1cf3ff0e52a75baf06aeeb0356b605a

Hide frame on control panel accelerator
Both frame and control panel are visible; by pressing F6 then
Alt-Shift-M.

Hide the frame when the accelerator is used, in the same way that the
buddy menu does hide the frame.

Regression introduced by e43d177.

src/jarabe/view/keyhandler.py
@@ -172,6 +172,9 @@ class KeyHandler(object):
     def handle_open_controlpanel(self, event_time):
         if shell.get_model().has_modal():
             return
+
+        self._frame.hide()
+
         panel = ControlPanel()
         panel.show()
 