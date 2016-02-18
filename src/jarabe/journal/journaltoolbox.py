151ece2418d21e50e242da8dfe03640bce9344f6
AbrahmAB <abhisandhyasp.ap@gmail.com>
2016-02-18 16:13:41 +0530

    Implemented toggled sorting order

--- src/jarabe/journal/journaltoolbox.py
+++ src/jarabe/journal/journaltoolbox.py
@@ -693,17 +693,13 @@ class SortingButton(ToolButton):
 
     def __init__(self):
         ToolButton.__init__(self)
-
         self._property = 'timestamp'
         self.order_type='ascending'
         self._order = Gtk.SortType.ASCENDING
-
         self.props.tooltip = _('Sort view')
         self.props.icon_name = 'view-lastedit'
-
         self.props.hide_tooltip_on_click = False
         self.palette_invoker.props.toggle_palette = True
-
         menu_box = PaletteMenuBox()
         self.props.palette.set_content(menu_box)
         menu_box.show()
@@ -727,6 +723,12 @@ class SortingButton(ToolButton):
                            icon)
             button.show()
             menu_box.append_item(button)
+        button = Gtk.ToggleButton('Reverse Order')
+        button.set_active(False)
+        button.connect('toggled',
+                       self.reverse_order)
+        button.show()
+        menu_box.append_item(button)
 
     def __sort_type_changed_cb(self, widget, property_, icon_name):
         if(self._property == property_):
@@ -738,17 +740,27 @@ class SortingButton(ToolButton):
              self.order_type='ascending'
 
         self._property = property_
+<<<<<<< HEAD
        
         if(self.order_type=='ascending'):
                    self._order = Gtk.SortType.ASCENDING
         else:
                    self._order=Gtk.SortType.DESCENDING
+=======
+        self._order = Gtk.SortType.ASCENDING
+>>>>>>> Implemented toggled sorting order
         self.emit('sort-property-changed')
-
         self.props.icon_name = icon_name
 
     def get_current_sort(self):
         return (self._property, self._order)
+
+    def reverse_order(self, button):
+        if button.get_active():
+            self._order = Gtk.SortType.DESCENDING
+        else:
+            self._order = Gtk.SortType.ASCENDING
+        self.emit('sort-property-changed')
 
 
 class EditToolbox(ToolbarBox):
