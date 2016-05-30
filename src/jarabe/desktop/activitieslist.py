sugarlabs/sugar

commit 161254959a8431686ec5a1a26657a34f96267ecc

This feature adds single-click-on-row-starts-activity to the list view.
Now the user can start an activity by just single clicking the row in the list view.

src/jarabe/desktop/activitieslist.py
@@ -56,6 +56,9 @@ class ActivitiesTreeView(Gtk.TreeView):
     def __init__(self):
         Gtk.TreeView.__init__(self)
         self.set_can_focus(False)
+        self.props.activate_on_single_click = True
+
+        self.connect('row-activated', self.__on_row_activated)
 
         self._query = ''
 
@@ -80,7 +83,6 @@ class ActivitiesTreeView(Gtk.TreeView):
             self.append_column(column)
 
         self.cell_icon = CellRendererActivityIcon()
-        self.cell_icon.connect('clicked', self.__icon_clicked_cb)
 
         column = Gtk.TreeViewColumn()
         column.pack_start(self.cell_icon, True)
@@ -154,7 +156,8 @@ class ActivitiesTreeView(Gtk.TreeView):
             not row[self._model.column_favorites[cell.favorite_view]],
             cell.favorite_view)
 
-    def __icon_clicked_cb(self, cell, path):
+    def __on_row_activated(self, treeview, path, col):
+        model = treeview.get_model()
         self._start_activity(path)
 
     def _start_activity(self, path):

commit 5a3776353ebf101ff2a1082c55e66803e5eed66a

Fix fail to start on Gtk+ 3.6
- fixes failure to start Sugar 0.109.0 on Gtk+ 3.6, where
  activate_on_single_click is not a property of a Gtk.TreeView,
  regression introduced by 161254959a8431686ec5a1a26657a34f96267ecc,

- restore removed icon clicked callback, and use with Gtk+ < 3.8,

- rename row activated method to follow convention (__*_cb).

src/jarabe/desktop/activitieslist.py
@@ -56,9 +56,6 @@ class ActivitiesTreeView(Gtk.TreeView):
     def __init__(self):
         Gtk.TreeView.__init__(self)
         self.set_can_focus(False)
-        self.props.activate_on_single_click = True
-
-        self.connect('row-activated', self.__on_row_activated)
 
         self._query = ''
 
@@ -139,6 +136,13 @@ class ActivitiesTreeView(Gtk.TreeView):
         self._invoker = TreeViewInvoker()
         self._invoker.attach_treeview(self)
 
+        if hasattr(self.props, 'activate_on_single_click'):
+            # Gtk+ 3.8 and later
+            self.props.activate_on_single_click = True
+            self.connect('row-activated', self.__row_activated_cb)
+        else:
+            self.cell_icon.connect('clicked', self.__icon_clicked_cb)
+
     def __favorite_set_data_cb(self, column, cell, model, tree_iter, data):
         favorite = \
             model[tree_iter][self._model.column_favorites[cell.favorite_view]]
@@ -156,10 +160,18 @@ class ActivitiesTreeView(Gtk.TreeView):
             not row[self._model.column_favorites[cell.favorite_view]],
             cell.favorite_view)
 
-    def __on_row_activated(self, treeview, path, col):
-        # Checks if the 'star' icon is clicked
+    def __icon_clicked_cb(self, cell, path):
+        """
+        A click on activity icon cell is to start an activity.
+        """
+        self._start_activity(path)
+
+    def __row_activated_cb(self, treeview, path, col):
+        """
+        A click on cells other than the favorite toggle is to start an
+        activity.  Gtk+ 3.8 and later.
+        """
         if col is not treeview.get_column(0):
-            model = treeview.get_model()
             self._start_activity(path)
 
     def _start_activity(self, path):