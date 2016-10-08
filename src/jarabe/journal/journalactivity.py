sugarlabs/sugar

commit 8be8b2a63d28930cf756de1d5cd53bd766565c52

Bind project list view to auto refresh
The _main_view_active boolean was refactored into an enum to better
accommodate how there is 3 views now (main, details and project).

Multi-selection was disabled in the project view.  It did not work
properly before this patch, due to the show_main_view call.

The show_main_view call from __selection_changed_cb was removed, as
this resulted in the main view being re-shown every refresh.  This call
did not appear to serve a purpose - selection changed would only be
called when the main view was focused (prior to adding the project
view).

src/jarabe/journal/journalactivity.py
@@ -182,6 +182,12 @@ class JournalActivityDBusService(dbus.service.Object):
         pass
 
 
+class JournalViews(object):
+    MAIN = 1
+    DETAIL = 2
+    PROJECT = 3
+
+
 class JournalActivity(JournalWindow):
 
     def __init__(self):
@@ -199,7 +205,7 @@ class JournalActivity(JournalWindow):
         self._detail_toolbox = None
         self._volumes_toolbar = None
         self._mount_point = '/'
-        self._main_view_active = True
+        self._active_view = JournalViews.MAIN
         self.project_metadata = None
         self._editing_mode = False
 
@@ -326,8 +332,7 @@ class JournalActivity(JournalWindow):
 
         self._project_view.connect('go-back-clicked',
                                    self.__go_back_clicked_cb)
-        self._main_view_active = False
-        logging.debug('project_view_activate signal handler')
+        self._active_view = JournalViews.PROJECT
         self.set_canvas(self._project_view)
         self._toolbox = self._main_toolbox
         self.set_toolbar_box(self._toolbox)
@@ -335,7 +340,6 @@ class JournalActivity(JournalWindow):
 
         query = {}
         query['project_id'] = self.project_metadata['uid']
-        #query['mountpoints'] = ['/']
         self._list_view_project.update_with_query(query)
         self._project_view.show_all()
 
@@ -365,13 +369,9 @@ class JournalActivity(JournalWindow):
         chooser.show_all()
 
     def __activity_selected_cb(self, widget, bundle_id, activity_id, title):
-        initialize_journal_object(title=title,
-                                  bundle_id=bundle_id,
-                                  activity_id=activity_id,
-                                  project_metadata=self.project_metadata)
-        query = {}
-        query['project_id'] = self.project_metadata['uid']
-        self._list_view_project.update_with_query(query)
+        initialize_journal_object(
+            title=title, bundle_id=bundle_id,
+            activity_id=activity_id, project_metadata=self.project_metadata)
 
     def __key_press_event_cb(self, widget, event):
         #if not self._main_toolbox.search_entry.has_focus():
@@ -388,7 +388,6 @@ class JournalActivity(JournalWindow):
         project_chooser.connect('response', self.__project_chooser_response_cb,
                                 metadata_to_send)
         project_chooser._toolbar._proj_list_button_clicked_cb(None)
-        logging.debug('__choose_project_cb')
 
     def __project_chooser_response_cb(self, project_chooser, response_value,
                                       metadata_to_send):
@@ -419,7 +418,6 @@ class JournalActivity(JournalWindow):
         self._editing_mode = selected_items != 0
         self._edit_toolbox.set_selected_entries(selected_items)
         self._edit_toolbox.display_selected_entries_status()
-        self.show_main_view()
 
     def update_selected_items_ui(self):
         selected_items = \
@@ -445,7 +443,7 @@ class JournalActivity(JournalWindow):
         self.connect('key-press-event', self.__key_press_event_cb)
 
     def show_main_view(self):
-        self._main_view_active = True
+        self._active_view = JournalViews.MAIN
         self.project_metadata = None
         if self._editing_mode:
             self._toolbox = self._edit_toolbox
@@ -462,7 +460,7 @@ class JournalActivity(JournalWindow):
             self._main_view.show()
 
     def _show_secondary_view(self, object_id):
-        self._main_view_active = False
+        self._active_view = JournalViews.DETAIL
         metadata = model.get(object_id)
         try:
             self._detail_toolbox.set_metadata(metadata)
@@ -515,22 +513,23 @@ class JournalActivity(JournalWindow):
             self.show_main_view()
 
     def _focus_in_event_cb(self, window, event):
-        if not self._main_view_active:
-            return
-        self._list_view.set_is_visible(True)
+        self._set_is_visible(True)
 
     def _focus_out_event_cb(self, window, event):
-        self._list_view.set_is_visible(False)
+        self._set_is_visible(False)
 
     def __window_state_event_cb(self, window, event):
         logging.debug('window_state_event_cb %r', self)
-        if not self._main_view_active:
-            return
-
         if event.changed_mask & Gdk.WindowState.ICONIFIED:
             state = event.new_window_state
             visible = not state & Gdk.WindowState.ICONIFIED
+            self._set_is_visible(visible)
+
+    def _set_is_visible(self, visible):
+        if self._active_view == JournalViews.MAIN:
             self._list_view.set_is_visible(visible)
+        elif self._active_view == JournalViews.PROJECT:
+            self._list_view_project.set_is_visible(visible)
 
     def _check_available_space(self):
         """Check available space on device

src/jarabe/journal/listview.py
@@ -235,6 +235,11 @@ class BaseListView(Gtk.Bin):
 
     def _is_new_item_visible(self, object_id):
         """Check if the created item is part of the currently selected view"""
+        if 'project_id' in self._query:
+            # TODO:  Would be best to check if the object_id is in the project.
+            #        But there is only ever 1 project listview, so it should
+            #        not be very costly.
+            return True
         if not self._query.get('mountpoints', None):
             return None
         if self._query['mountpoints'] == ['/']:

src/jarabe/journal/projectview.py
@@ -80,7 +80,7 @@ class ProjectView(Gtk.EventBox, BaseExpandedEntry):
         return self._vbox
 
     def create_list_view_project(self):
-        self._list_view_project = ListView(self, enable_multi_operations=True)
+        self._list_view_project = ListView(self)
         return self._list_view_project
 
     def get_list_view(self):
@@ -99,10 +99,6 @@ class ProjectView(Gtk.EventBox, BaseExpandedEntry):
         description = project_metadata.get('description', '')
         self._description.get_buffer().set_text(description)
         self._title.set_text(project_metadata.get('title', ''))
-
-    def _add_buddy_button_clicked_cb(self, button):
-        #TODO: TO be implemented
-        pass
 
     def _title_focus_out_event_cb(self, entry, event):
         self._update_entry()

commit 14044859170f12972324277800566ba312e39cba

Fix for journal batch operations
In 0.108 and earlier, the edit toolbar is shown when journal items are
selected.

In 0.110 the edit toolbar does not appear, so operations such as batch
erase are not available.

Regression was probably introduced by 8be8b2a ('Bind project list view
to auto refresh') and other patches since 0.108.

References:

https://wiki.sugarlabs.org/go/Features/Multi_selection (2013)

src/jarabe/journal/journalactivity.py
@@ -418,6 +418,7 @@ class JournalActivity(JournalWindow):
         self._editing_mode = selected_items != 0
         self._edit_toolbox.set_selected_entries(selected_items)
         self._edit_toolbox.display_selected_entries_status()
+        self.show_main_view()
 
     def update_selected_items_ui(self):
         selected_items = \