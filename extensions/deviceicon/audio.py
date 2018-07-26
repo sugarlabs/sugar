sugarlabs/sugar

commit 9ec9b991263b5d3e3f0acefda8c383a65025a0a2

Port from GObject to GLib

extensions/deviceicon/audio.py
@@ -238,9 +238,9 @@ class AudioPalette(Palette):
 
 class DeviceModelAudio(GObject.GObject):
     __gproperties__ = {
-        'level': (int, None, None, 0, 100, 0, GObject.PARAM_READWRITE),
-        'last-level': (int, None, None, 0, 100, 0, GObject.PARAM_READABLE),
-        'muted': (bool, None, None, False, GObject.PARAM_READWRITE),
+        'level': (int, None, None, 0, 100, 0, GObject.ParamFlags.READWRITE),
+        'last-level': (int, None, None, 0, 100, 0, GObject.ParamFlags.READABLE),
+        'muted': (bool, None, None, False, GObject.ParamFlags.READWRITE),
     }
 
     def __init__(self):

src/jarabe/controlpanel/sectionview.py
@@ -30,9 +30,9 @@ class SectionView(Gtk.VBox):
     }
 
     __gproperties__ = {
-        'is_valid': (bool, None, None, True, GObject.PARAM_READWRITE),
-        'is_cancellable': (bool, None, None, True, GObject.PARAM_READWRITE),
-        'is_deferrable': (bool, None, None, True, GObject.PARAM_READWRITE),
+        'is_valid': (bool, None, None, True, GObject.ParamFlags.READWRITE),
+        'is_cancellable': (bool, None, None, True, GObject.ParamFlags.READWRITE),
+        'is_deferrable': (bool, None, None, True, GObject.ParamFlags.READWRITE),
     }
 
     _APPLY_TIMEOUT = 1000

src/jarabe/model/shell.py
@@ -109,7 +109,7 @@ class Activity(GObject.GObject):
     def get_launch_status(self):
         return self._launch_status
 
-    launch_status = GObject.property(getter=get_launch_status)
+    launch_status = GObject.Property(getter=get_launch_status)
 
     def add_window(self, window, is_main_window=False):
         """Add a window to the windows stack."""
@@ -731,10 +731,10 @@ class ShellModel(GObject.GObject):
         self.emit('launch-started', home_activity)
 
         if activity_id in self._launch_timers:
-            GObject.source_remove(self._launch_timers[activity_id])
+            GLib.source_remove(self._launch_timers[activity_id])
             del self._launch_timers[activity_id]
 
-        timer = GObject.timeout_add_seconds(90, self._check_activity_launched,
+        timer = GLib.timeout_add_seconds(90, self._check_activity_launched,
                                             activity_id)
         self._launch_timers[activity_id] = timer
 

extensions/cpsection/aboutme/view.py
@@ -19,6 +19,7 @@
 
 from gi.repository import Gtk
 from gi.repository import GObject
+from gi.repository import GLib
 from gettext import gettext as _
 
 from sugar3.graphics import style
@@ -143,7 +144,7 @@ class ColorPicker(EventIcon):
             self._color = color
         self.props.xo_color = self._color
 
-    color = GObject.property(type=object, setter=set_color)
+    color = GObject.Property(type=object, setter=set_color)
 
     def __activate_cb(self, button, picker):
         if picker != _CURRENT_COLOR:
@@ -365,9 +366,9 @@ class AboutMe(SectionView):
 
     def __nick_changed_cb(self, widget, data=None):
         if self._nick_sid:
-            GObject.source_remove(self._nick_sid)
-        self._nick_sid = GObject.timeout_add(self._APPLY_TIMEOUT,
-                                             self.__nick_timeout_cb, widget)
+            GLib.source_remove(self._nick_sid)
+        self._nick_sid = GLib.timeout_add(self._APPLY_TIMEOUT,
+                                          self.__nick_timeout_cb, widget)
 
     def __nick_timeout_cb(self, widget):
         self._nick_sid = 0

extensions/cpsection/background/view.py
@@ -19,7 +19,7 @@ import os
 
 from gi.repository import Gtk
 from gi.repository import Gdk
-from gi.repository import GObject
+from gi.repository import GLib
 from gi.repository import GdkPixbuf
 
 from sugar3.graphics import style
@@ -129,14 +129,14 @@ class Background(SectionView):
                 pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
                     file_path, style.XLARGE_ICON_SIZE,
                     style.XLARGE_ICON_SIZE)
-            except GObject.GError:
+            except GLib.GError:
                 pass
             else:
                 self._store.append([pixbuf, file_path])
                 self._paths_list.append(file_path)
 
-            self._append_to_store_sid = GObject.idle_add(self._append_to_store,
-                                                         file_paths)
+            self._append_to_store_sid = GLib.idle_add(self._append_to_store,
+                                                      file_paths)
         else:
             self._select_background()
             self._images_loaded = True
@@ -147,7 +147,7 @@ class Background(SectionView):
 
     def _cancel_append_to_store(self):
         if self._append_to_store_sid is not None:
-            GObject.source_remove(self._append_to_store_sid)
+            GLib.source_remove(self._append_to_store_sid)
             self._append_to_store_sid = None
 
     def __realize_cb(self, widget):

extensions/cpsection/backup/backends/volume.py
@@ -22,7 +22,7 @@ from datetime import datetime
 
 from gettext import gettext as _
 from gi.repository import Gio
-from gi.repository import GObject
+from gi.repository import GLib
 
 from sugar3 import env
 from sugar3 import profile
@@ -105,7 +105,7 @@ class Backup(Backend):
         elif len(self._entries) == 0:
             self._do_finish()
         else:
-            GObject.idle_add(self._do_continue)
+            GLib.idle_add(self._do_continue)
 
     def _do_cancel(self):
         self._tarfile.close()
@@ -133,7 +133,7 @@ class Backup(Backend):
         self._entries = self._get_datastore_entries()
         self._total = len(self._entries)
         self._cancelled = False
-        GObject.idle_add(self._do_continue)
+        GLib.idle_add(self._do_continue)
 
     def cancel(self):
         self._cancelled = True
@@ -218,7 +218,7 @@ class Restore(Backend):
                 self._percent = percent
                 logging.debug('restore-local progress is %f', percent)
                 self.emit('progress', float(percent) / 100.0)
-            GObject.idle_add(self._do_continue)
+            GLib.idle_add(self._do_continue)
         else:
             self._do_finish()
 
@@ -234,7 +234,7 @@ class Restore(Backend):
         self._tarfile = tarfile.open(self._checkpoint, 'r:gz')
         self._bytes = 0.0
         self._reset_datastore()
-        GObject.idle_add(self._do_continue)
+        GLib.idle_add(self._do_continue)
 
     def cancel(self):
         if not self._cancellable:

extensions/cpsection/backup/view.py
@@ -17,6 +17,7 @@ import logging
 from gettext import gettext as _
 
 from gi.repository import GObject
+from gi.repository import GLib
 from gi.repository import Gtk
 from gi.repository import Gdk
 
@@ -63,9 +64,9 @@ class BackupView(SectionView):
 class _BackupButton(Gtk.EventBox):
 
     __gproperties__ = {
-        'icon-name': (str, None, None, None, GObject.PARAM_READWRITE),
-        'pixel-size': (object, None, None, GObject.PARAM_READWRITE),
-        'title': (str, None, None, None, GObject.PARAM_READWRITE),
+        'icon-name': (str, None, None, None, GObject.ParamFlags.READWRITE),
+        'pixel-size': (object, None, None, GObject.ParamFlags.READWRITE),
+        'title': (str, None, None, None, GObject.ParamFlags.READWRITE),
     }
 
     def __init__(self, **kwargs):
@@ -237,7 +238,7 @@ class OperationPanel(Gtk.Grid):
             self._ask_options(message, combo_options, self._select_backend)
         else:
             self._backend = self._view.manager.get_backends()[0]
-            GObject.idle_add(self._start_operation)
+            GLib.idle_add(self._start_operation)
 
     def _ask_options(self, message, options, continue_cb):
         """
@@ -291,7 +292,7 @@ class OperationPanel(Gtk.Grid):
             self._operator = self._backend.get_restore()
             self._message_label.set_text(_('Starting restore...'))
 
-        GObject.idle_add(self._continue_operation)
+        GLib.idle_add(self._continue_operation)
 
     def _continue_operation(self, options={}):
         need_more_information = True
@@ -306,7 +307,7 @@ class OperationPanel(Gtk.Grid):
                               self._assign_parameter_backend)
         if not need_more_information:
             if self._operation == OPERATION_BACKUP:
-                GObject.idle_add(self._internal_start_operation)
+                GLib.idle_add(self._internal_start_operation)
 
             if self._operation == OPERATION_RESTORE:
                 # The restore is potentially dangerous
@@ -347,7 +348,7 @@ class OperationPanel(Gtk.Grid):
         # disable the accept button until the operation finish
         self._view.props.is_valid = False
 
-        GObject.idle_add(self._operator.start)
+        GLib.idle_add(self._operator.start)
 
     def __retry_cb(self, button):
         self._start_operation()

extensions/cpsection/datetime/view.py
@@ -16,6 +16,7 @@
 
 from gi.repository import Gtk
 from gi.repository import GObject
+from gi.repository import GLib
 from gettext import gettext as _
 
 from sugar3.graphics import style
@@ -121,9 +122,9 @@ class TimeZone(SectionView):
             return False
 
         if self._zone_sid:
-            GObject.source_remove(self._zone_sid)
-        self._zone_sid = GObject.timeout_add(self._APPLY_TIMEOUT,
-                                             self.__zone_timeout_cb, row)
+            GLib.source_remove(self._zone_sid)
+        self._zone_sid = GLib.timeout_add(self._APPLY_TIMEOUT,
+                                          self.__zone_timeout_cb, row)
         return True
 
     def __zone_timeout_cb(self, row):

commit 02e702e5e9ccd2904f016d34cbce8fa0813a88db

Review: Port from GObject to GLib
Fix new flake8 issues introduced by 9ec9b99.

extensions/deviceicon/audio.py
@@ -239,7 +239,8 @@ class AudioPalette(Palette):
 class DeviceModelAudio(GObject.GObject):
     __gproperties__ = {
         'level': (int, None, None, 0, 100, 0, GObject.ParamFlags.READWRITE),
-        'last-level': (int, None, None, 0, 100, 0, GObject.ParamFlags.READABLE),
+        'last-level': (int, None, None, 0, 100, 0,
+                       GObject.ParamFlags.READABLE),
         'muted': (bool, None, None, False, GObject.ParamFlags.READWRITE),
     }
 

src/jarabe/controlpanel/sectionview.py
@@ -31,8 +31,10 @@ class SectionView(Gtk.VBox):
 
     __gproperties__ = {
         'is_valid': (bool, None, None, True, GObject.ParamFlags.READWRITE),
-        'is_cancellable': (bool, None, None, True, GObject.ParamFlags.READWRITE),
-        'is_deferrable': (bool, None, None, True, GObject.ParamFlags.READWRITE),
+        'is_cancellable': (bool, None, None, True,
+                           GObject.ParamFlags.READWRITE),
+        'is_deferrable': (bool, None, None, True,
+                          GObject.ParamFlags.READWRITE),
     }
 
     _APPLY_TIMEOUT = 1000

src/jarabe/model/shell.py
@@ -735,7 +735,7 @@ class ShellModel(GObject.GObject):
             del self._launch_timers[activity_id]
 
         timer = GLib.timeout_add_seconds(90, self._check_activity_launched,
-                                            activity_id)
+                                         activity_id)
         self._launch_timers[activity_id] = timer
 
     def notify_launch_failed(self, activity_id):