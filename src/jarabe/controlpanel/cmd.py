sugarlabs/sugar

commit e4e2d870c18fabcbb215ce97e01211e90a3302fc

Localize unnamed arguments with named arguments and mapping
* xgettext is unable to reorder unnamed arguments.

* Issue fixed by using named arguments.

Reference: https://stackoverflow.com/questions/23405521/why-i-get-msgid-format-string-unnamed-arguments-warning-only-when-running-m

Fixes #807

Tested-by: Rahul Bothra <rrbothra@gmail.com>  # Ubuntu 16.04
Signed-off-by: James Cameron <quozl@laptop.org>  # Rebase, reword

src/jarabe/controlpanel/cmd.py
@@ -25,7 +25,8 @@ from jarabe import config
 _RESTART = 1
 
 _same_option_warning = _('sugar-control-panel: WARNING, found more than one'
-                         ' option with the same name: %s module: %r')
+                         ' option with the same name: %(key)s'
+                         ' module: %(module)r')
 _no_option_error = _('sugar-control-panel: key=%s not an available option')
 _general_error = _('sugar-control-panel: %s')
 
@@ -97,7 +98,8 @@ def main():
                     if found == 1:
                         print method.__doc__
                     else:
-                        print _(_same_option_warning % (key, module))
+                        print _(_same_option_warning
+                                % ('key': key, 'module': module))
             if found == 0:
                 print _(_no_option_error % key)
         if option in ('-l'):
@@ -121,7 +123,8 @@ def main():
                         except Exception as detail:
                             print _(_general_error % detail)
                     else:
-                        print _(_same_option_warning % (key, module))
+                        print _(_same_option_warning %
+                                ('key': key, 'module': module))
             if found == 0:
                 print _(_no_option_error % key)
         if option in ('-s'):
@@ -138,7 +141,8 @@ def main():
                         if note == _RESTART:
                             note_restart()
                     else:
-                        print _(_same_option_warning % (key, module))
+                        print _(_same_option_warning %
+                                ('key': key, 'module': module))
             if found == 0:
                 print _(_no_option_error % key)
         if option in ('-c'):
@@ -155,6 +159,7 @@ def main():
                         if note == _RESTART:
                             note_restart()
                     else:
-                        print _(_same_option_warning % (key, module))
+                        print _(_same_option_warning %
+                                ('key': key, 'module': module))
             if found == 0:
                 print _(_no_option_error % key)

po/sugar.pot
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2018-09-21 14:29+1000\n"
+"POT-Creation-Date: 2018-12-10 22:42+0530\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -209,7 +209,7 @@ msgid ""
 msgstr ""
 
 #: extensions/cpsection/backup/view.py:323
-#: src/jarabe/frame/activitiestray.py:655
+#: src/jarabe/frame/activitiestray.py:657
 msgid "Accept"
 msgstr ""
 
@@ -809,9 +809,9 @@ msgid "Disconnected"
 msgstr ""
 
 #: extensions/deviceicon/network.py:303 src/jarabe/controlpanel/toolbar.py:122
-#: src/jarabe/frame/activitiestray.py:700
-#: src/jarabe/frame/activitiestray.py:822
-#: src/jarabe/frame/activitiestray.py:855
+#: src/jarabe/frame/activitiestray.py:702
+#: src/jarabe/frame/activitiestray.py:824
+#: src/jarabe/frame/activitiestray.py:857
 #: src/jarabe/journal/expandedentry.py:160
 #: src/jarabe/journal/journaltoolbox.py:613 src/jarabe/journal/palettes.py:192
 msgid "Cancel"
@@ -920,22 +920,22 @@ msgstr ""
 #, python-format
 msgid ""
 "sugar-control-panel: WARNING, found more than one option with the same name: "
-"%s module: %r"
+"%(key)s module: %(module)r"
 msgstr ""
 
-#: src/jarabe/controlpanel/cmd.py:29
+#: src/jarabe/controlpanel/cmd.py:30
 #, python-format
 msgid "sugar-control-panel: key=%s not an available option"
 msgstr ""
 
-#: src/jarabe/controlpanel/cmd.py:30
+#: src/jarabe/controlpanel/cmd.py:31
 #, python-format
 msgid "sugar-control-panel: %s"
 msgstr ""
 
 #. TRANS: Translators, there's a empty line at the end of this string,
 #. which must appear in the translated string (msgstr) as well.
-#: src/jarabe/controlpanel/cmd.py:37
+#: src/jarabe/controlpanel/cmd.py:38
 msgid ""
 "Usage: sugar-control-panel [ option ] key [ args ... ] \n"
 "    Control for the sugar environment. \n"
@@ -949,7 +949,7 @@ msgid ""
 "    "
 msgstr ""
 
-#: src/jarabe/controlpanel/cmd.py:51
+#: src/jarabe/controlpanel/cmd.py:52
 msgid ""
 "To apply your changes you have to restart Sugar.\n"
 "Hit ctrl+alt+erase on the keyboard to trigger a restart."
@@ -1188,7 +1188,7 @@ msgid "<Ctrl>%d"
 msgstr ""
 
 #: src/jarabe/frame/activitiestray.py:213
-#: src/jarabe/frame/activitiestray.py:664
+#: src/jarabe/frame/activitiestray.py:666
 msgid "Decline"
 msgstr ""
 
@@ -1203,27 +1203,27 @@ msgid "%d MiB"
 msgstr ""
 
 #. TRANS: file transfer, bytes transferred, e.g. 128 of 1024
-#: src/jarabe/frame/activitiestray.py:627
+#: src/jarabe/frame/activitiestray.py:628
 #, python-format
-msgid "%s of %s"
+msgid "%(transferred)s of %(total)s"
 msgstr ""
 
-#: src/jarabe/frame/activitiestray.py:641
+#: src/jarabe/frame/activitiestray.py:643
 #, python-format
 msgid "Transfer from %s"
 msgstr ""
 
-#: src/jarabe/frame/activitiestray.py:729
-#: src/jarabe/frame/activitiestray.py:743
-#: src/jarabe/frame/activitiestray.py:885
+#: src/jarabe/frame/activitiestray.py:731
+#: src/jarabe/frame/activitiestray.py:745
+#: src/jarabe/frame/activitiestray.py:887
 msgid "Dismiss"
 msgstr ""
 
-#: src/jarabe/frame/activitiestray.py:757
+#: src/jarabe/frame/activitiestray.py:759
 msgid "The other participant canceled the file transfer"
 msgstr ""
 
-#: src/jarabe/frame/activitiestray.py:807
+#: src/jarabe/frame/activitiestray.py:809
 #, python-format
 msgid "Transfer to %s"
 msgstr ""
@@ -1407,11 +1407,11 @@ msgstr ""
 msgid "Journal"
 msgstr ""
 
-#: src/jarabe/journal/journalactivity.py:285
+#: src/jarabe/journal/journalactivity.py:286
 msgid "Add new project"
 msgstr ""
 
-#: src/jarabe/journal/journalactivity.py:364
+#: src/jarabe/journal/journalactivity.py:365
 #, python-format
 msgid "Choose an activity to start '%s' with"
 msgstr ""
@@ -1859,15 +1859,17 @@ msgstr ""
 msgid "Activity launcher"
 msgstr ""
 
-#: src/jarabe/view/alerts.py:38
-#, python-format
-msgid "%s is already running. Please stop %s before launching it again."
-msgstr ""
-
 #: src/jarabe/view/alerts.py:48
 msgid ""
 "The maximum number of open activities has been reached. Please close an "
 "activity before launching a new one."
+msgstr ""
+
+#: src/jarabe/view/alerts.py:38
+#, python-format
+msgid ""
+"%(activity)s is already running. Please stop %(activity)s before launching "
+"it again."
 msgstr ""
 
 #: src/jarabe/view/buddymenu.py:74

src/jarabe/frame/activitiestray.py
@@ -624,7 +624,9 @@ class BaseTransferPalette(Palette):
             self.file_transfer.props.transferred_bytes)
         total = self._format_size(self.file_transfer.file_size)
         # TRANS: file transfer, bytes transferred, e.g. 128 of 1024
-        self.progress_label.props.label = _('%s of %s') % (transferred, total)
+        self.progress_label.props.label = \
+            _('%(transferred)s of %(total)s') \
+            % ({'transferred': transferred, 'total': total})
 
 
 class IncomingTransferPalette(BaseTransferPalette):

src/jarabe/view/alerts.py
@@ -35,8 +35,8 @@ class MultipleInstanceAlert(BaseErrorAlert):
         BaseErrorAlert.__init__(
             self,
             _('Activity launcher'),
-            _('%s is already running. \
-Please stop %s before launching it again.' % (name, name)))
+            _('%(activity)s is already running. \
+Please stop %(activity)s before launching it again.' % ({'activity':name})
 
 
 class MaxOpenActivitiesAlert(BaseErrorAlert):

commit 4ac9899b76657d5cca0435a03f8c8db2ac55835b

Localize unnamed arguments with named arguments and mapping - invalid syntax 2
During install of an Ubuntu package;

  File "/usr/lib/python2.7/dist-packages/jarabe/controlpanel/cmd.py", line 102
    % ('key': key, 'module': module))
            ^
SyntaxError: invalid syntax

Regression introduced by e4e2d87.

Also removed double translation, and a message that will never be
translated.

src/jarabe/controlpanel/cmd.py
@@ -28,7 +28,7 @@ _same_option_warning = _('sugar-control-panel: WARNING, found more than one'
                          ' option with the same name: %(key)s'
                          ' module: %(module)r')
 _no_option_error = _('sugar-control-panel: key=%s not an available option')
-_general_error = _('sugar-control-panel: %s')
+_general_error = 'sugar-control-panel: %s'
 
 
 def cmd_help():
@@ -98,10 +98,10 @@ def main():
                     if found == 1:
                         print method.__doc__
                     else:
-                        print _(_same_option_warning
-                                % ('key': key, 'module': module))
+                        print _same_option_warning % \
+                            {'key': key, 'module': module}
             if found == 0:
-                print _(_no_option_error % key)
+                print _no_option_error % key
         if option in ('-l'):
             for module in modules:
                 methods = dir(module)
@@ -121,12 +121,12 @@ def main():
                         try:
                             method()
                         except Exception as detail:
-                            print _(_general_error % detail)
+                            print _general_error % detail
                     else:
-                        print _(_same_option_warning %
-                                ('key': key, 'module': module))
+                        print _same_option_warning % \
+                            {'key': key, 'module': module}
             if found == 0:
-                print _(_no_option_error % key)
+                print _no_option_error % key
         if option in ('-s'):
             for module in modules:
                 method = getattr(module, 'set_' + key, None)
@@ -137,14 +137,14 @@ def main():
                         try:
                             note = method(*args)
                         except Exception as detail:
-                            print _(_general_error % detail)
+                            print _general_error % detail
                         if note == _RESTART:
                             note_restart()
                     else:
-                        print _(_same_option_warning %
-                                ('key': key, 'module': module))
+                        print _same_option_warning % \
+                            {'key': key, 'module': module}
             if found == 0:
-                print _(_no_option_error % key)
+                print _no_option_error % key
         if option in ('-c'):
             for module in modules:
                 method = getattr(module, 'clear_' + key, None)
@@ -155,11 +155,11 @@ def main():
                         try:
                             note = method(*args)
                         except Exception as detail:
-                            print _(_general_error % detail)
+                            print _general_error % detail
                         if note == _RESTART:
                             note_restart()
                     else:
-                        print _(_same_option_warning %
-                                ('key': key, 'module': module))
+                        print _same_option_warning % \
+                            {'key': key, 'module': module}
             if found == 0:
-                print _(_no_option_error % key)
+                print _no_option_error % key