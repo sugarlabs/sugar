2cf7ef551e1bd229798ecba321ce663018e14ee7
James Cameron <quozl@laptop.org>
2019-09-03 18:01:56 +1000

    Fix hang of About my Computer
    Another Port to Python 3 regression, changes were made without searching
    for all similar scenarios.
    
    Fixes https://github.com/sugarlabs/sugar/issues/854

--- extensions/cpsection/aboutcomputer/model.py
+++ extensions/cpsection/aboutcomputer/model.py
@@ -58,7 +58,7 @@ def _get_serial_number():
     if status != 0:
         return _not_available
 
-    return output.rstrip('\n')
+    return output.decode().rstrip('\n')
 
 
 def get_serial_number():
@@ -179,6 +179,7 @@ def get_wireless_firmware():
 
         try:
             for line in output:
+                line = line.decode()
                 if line.startswith('firmware'):
                     version = line.split()[1]
                 if line.startswith('driver'):
