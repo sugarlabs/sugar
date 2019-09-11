sugarlabs/sugar

commit aa18879e9717dfe2d30f249549e9a43d6dd6da4f

Port to Python 3
* remove call to sys.setdefaultencoding
  default encoding is 'utf-8' by default in Python 3

* remove use of statvfs
  statvfs does not exist in python 3

* TODO was fixed in af6ce2e

* Remove unused function

* flake fixes

* Fix unicode encoding error

* Use a list to iterate over, supporting Python 3

* Encode 'str' instance into 'utf-8'

Co-authored-by: Aniket21mathur <aniketmathur320@gmail.com>

src/jarabe/journal/model.py
@@ -291,9 +291,8 @@ class InplaceResultSet(BaseResultSet):
         else:
             # timestamp
             keygetter = itemgetter(2)
-        self._file_list.sort(lambda a, b: cmp(b, a),
-                             key=keygetter,
-                             reverse=(self._sort[0] == '-'))
+        self._file_list.sort(key=keygetter,
+                             reverse=not self._sort[0] == '-')
         self.ready.send(self)
 
     def find(self, query):
@@ -949,7 +948,7 @@ def get_documents_path():
     try:
         pipe = subprocess.Popen(['xdg-user-dir', 'DOCUMENTS'],
                                 stdout=subprocess.PIPE)
-        documents_path = os.path.normpath(pipe.communicate()[0].strip())
+        documents_path = os.path.normpath(pipe.communicate()[0].strip().decode())
         if os.path.exists(documents_path) and \
                 os.environ.get('HOME') != documents_path:
             _documents_path = documents_path

extensions/cpsection/aboutcomputer/model.py
@@ -45,7 +45,7 @@ def get_aboutcomputer():
 
 
 def print_aboutcomputer():
-    print get_aboutcomputer()
+    print(get_aboutcomputer())
 
 
 def _get_serial_number():
@@ -74,7 +74,7 @@ def print_serial_number():
     serial_no = get_serial_number()
     if serial_no is None:
         serial_no = _not_available
-    print serial_no
+    print(serial_no)
 
 
 def get_build_number():
@@ -87,12 +87,12 @@ def get_build_number():
         try:
             popen = subprocess.Popen(['lsb_release', '-ds'],
                                      stdout=subprocess.PIPE)
-        except OSError, e:
+        except OSError as e:
             if e.errno != errno.ENOENT:
                 raise
         else:
             build_no, stderr_ = popen.communicate()
-            build_no = build_no.strip()
+            build_no = build_no.strip().decode()
 
     if build_no is None or not build_no:
         build_no = _not_available
@@ -101,7 +101,7 @@ def get_build_number():
 
 
 def print_build_number():
-    print get_build_number()
+    print(get_build_number())
 
 
 def get_firmware_number():
@@ -154,7 +154,7 @@ def get_secondary_licenses():
 
 
 def print_firmware_number():
-    print get_firmware_number()
+    print(get_firmware_number())
 
 
 def get_wireless_firmware():
@@ -206,15 +206,15 @@ def get_wireless_firmware():
         return _not_available
 
     if len(firmware_info) == 1:
-        return firmware_info.values()[0]
+        return list(firmware_info.values())[0]
 
     return ', '.join(['%(interface)s: %(info)s' %
                       {'interface': interface, 'info': info}
-                      for interface, info in firmware_info.items()])
+                      for interface, info in list(firmware_info.items())])
 
 
 def print_wireless_firmware():
-    print get_wireless_firmware()
+    print(get_wireless_firmware())
 
 
 def _read_file(path):

extensions/cpsection/aboutme/model.py
@@ -39,7 +39,7 @@ def get_nick():
 
 
 def print_nick():
-    print get_nick()
+    print(get_nick())
 
 
 def set_nick(nick):
@@ -48,8 +48,8 @@ def set_nick(nick):
     """
     if not nick:
         raise ValueError(_('You must enter a name.'))
-    if not isinstance(nick, unicode):
-        nick = unicode(nick, 'utf-8')
+    if not isinstance(nick, str):
+        nick = str(nick, 'utf-8')
     settings = Gio.Settings('org.sugarlabs.user')
     settings.set_string('nick', nick)
     return 1
@@ -74,14 +74,14 @@ def print_color():
                 fill_tuple = (color, hue)
 
     if stroke_tuple is not None:
-        print ('stroke:   color=%s hue=%s') % (stroke_tuple[0],
-                                               stroke_tuple[1])
+        print(('stroke:   color=%s hue=%s') % (stroke_tuple[0],
+                                               stroke_tuple[1]))
     else:
-        print ('stroke:   %s') % (tmp[0])
+        print(('stroke:   %s') % (tmp[0]))
     if fill_tuple is not None:
-        print ('fill:     color=%s hue=%s') % (fill_tuple[0], fill_tuple[1])
+        print(('fill:     color=%s hue=%s') % (fill_tuple[0], fill_tuple[1]))
     else:
-        print ('fill:     %s') % (tmp[1])
+        print(('fill:     %s') % (tmp[1]))
 
 
 def set_color(stroke, fill, stroke_modifier='medium', fill_modifier='medium'):
@@ -93,10 +93,10 @@ def set_color(stroke, fill, stroke_modifier='medium', fill_modifier='medium'):
     """
 
     if stroke_modifier not in _MODIFIERS or fill_modifier not in _MODIFIERS:
-        print (_('Error in specified color modifiers.'))
+        print((_('Error in specified color modifiers.')))
         return
     if stroke not in _COLORS or fill not in _COLORS:
-        print (_('Error in specified colors.'))
+        print((_('Error in specified colors.')))
         return
 
     if stroke_modifier == fill_modifier:

extensions/cpsection/datetime/model.py
@@ -50,7 +50,7 @@ def read_all_timezones(fn=_zone_tab):
             timezones.append(line[2])
     timezones.sort()
 
-    for offset in xrange(-12, 15):
+    for offset in range(-12, 15):
         if offset < 0:
             tz = 'UTC%d' % offset
         elif offset > 0:
@@ -67,7 +67,7 @@ def get_timezone():
 
 
 def print_timezone():
-    print get_timezone()
+    print(get_timezone())
 
 
 def fix_UTC_time_zone(timezone):

extensions/cpsection/frame/model.py
@@ -25,7 +25,7 @@ def get_corner_delay():
 
 
 def print_corner_delay():
-    print get_corner_delay()
+    print(get_corner_delay())
 
 
 def set_corner_delay(delay):
@@ -44,7 +44,7 @@ def get_edge_delay():
 
 
 def print_edge_delay():
-    print get_edge_delay()
+    print(get_edge_delay())
 
 
 def set_edge_delay(delay):
@@ -63,7 +63,7 @@ def get_trigger_size():
 
 
 def print_trigger_size():
-    print '{}px'.format(get_trigger_size())
+    print('{}px'.format(get_trigger_size()))
 
 
 def set_trigger_size(size):

extensions/cpsection/language/model.py
@@ -30,7 +30,7 @@ _standard_msg = _('Could not access ~/.i18n. Create standard settings.')
 
 def read_all_languages():
     fdp = subprocess.Popen(['locale', '-av'], stdout=subprocess.PIPE)
-    lines = fdp.stdout.read().split('\n')
+    lines = fdp.stdout.read().decode().split('\n')
     locales = []
 
     for line in lines:
@@ -142,12 +142,12 @@ def print_languages():
         found_lang = False
         for lang in languages:
             if lang[2].split('.')[0] == code.split('.')[0]:
-                print lang[0].replace(' ', '_') + '/' + \
-                    lang[1].replace(' ', '_')
+                print(lang[0].replace(' ', '_') + '/' +
+                      lang[1].replace(' ', '_'))
                 found_lang = True
                 break
         if not found_lang:
-            print (_('Language for code=%s could not be determined.') % code)
+            print((_('Language for code=%s could not be determined.') % code))
 
 
 def set_languages(languages):
@@ -170,7 +170,7 @@ def set_languages(languages):
             if code == languages:
                 set_languages_list([locale_str])
                 return 1
-        print (_("Sorry I do not speak \'%s\'.") % languages)
+        print((_("Sorry I do not speak \'%s\'.") % languages))
 
 
 def set_languages_list(languages):

extensions/cpsection/modemconfiguration/model.py
@@ -215,7 +215,7 @@ class ServiceProvidersParser(object):
         # Check service provider database file exists
         try:
             tree = ElementTree(file=PROVIDERS_PATH)
-        except (IOError, SyntaxError), e:
+        except (IOError, SyntaxError) as e:
             msg = ("Mobile broadband provider database: Could not read "
                    "provider information %s error=%s") % (PROVIDERS_PATH, e)
             logging.warning(msg)
@@ -317,7 +317,7 @@ class ServiceProviders(object):
 
         provider_name = self._settings.get_string(CONF_SP_PROVIDER)
         if not provider_name:
-            provider_name = u''
+            provider_name = ''
         else:
             provider_name = provider_name.decode('utf-8')
 

extensions/cpsection/network/model.py
@@ -15,8 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import logging
-
 import gi
 gi.require_version('NM', '1.0')
 from gettext import gettext as _
@@ -62,7 +60,7 @@ def get_jabber():
 
 
 def print_jabber():
-    print get_jabber()
+    print(get_jabber())
 
 
 def set_jabber(server):
@@ -82,7 +80,7 @@ def get_radio():
 
 
 def print_radio():
-    print ('off', 'on')[get_radio()]
+    print(('off', 'on')[get_radio()])
 
 
 def set_radio(state):
@@ -119,7 +117,7 @@ def get_publish_information():
 
 
 def print_publish_information():
-    print get_publish_information()
+    print(get_publish_information())
 
 
 def set_publish_information(value):

commit babf1a5a28e5ccfb2909c142c821dc860287ca5e

Fix filesystem preview read
Visiting filesystem objects did not display preview.

Binary file was being read as text.

Regression introduced by aa18879e9 ("Port to Python 3").

src/jarabe/journal/model.py
@@ -533,7 +533,8 @@ def _get_file_metadata_from_json(path, fetch_preview):
     else:
         if os.path.exists(preview_path):
             try:
-                metadata['preview'] = dbus.ByteArray(open(preview_path).read())
+                metadata['preview'] = dbus.ByteArray(
+                    open(preview_path, 'rb').read())
             except EnvironmentError:
                 logging.debug('Could not read preview for file %r on '
                               'external device.', filename)