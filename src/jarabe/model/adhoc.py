sugarlabs/sugar

commit 5ff30eadf670f5225ae305f10e248c7d97a88fb4

Add default Ad-hoc networks #1610
This patch adds three default Ad-hoc networks, for channel 1, 6 and 11. They
are represented with designated icons in the neighborhood view. This will
mimic the mesh behavior on devices where mesh hardware is not available
and make the "under a tree"-scenario possible in those cases. If Sugar sees
no "known" network when it starts, it does autoconnect to an Ad-hoc network.

http://wiki.sugarlabs.org/go/Features/Sugar_Adhoc_Networks

src/jarabe/model/adhoc.py
@@ -1,0 +1,299 @@
+# Copyright (C) 2010 One Laptop per Child
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import logging
+
+import dbus
+import gobject
+
+from jarabe.model import network
+from jarabe.model.network import Settings
+from sugar.util import unique_id
+from jarabe.model.network import IP4Config
+
+_NM_SERVICE = 'org.freedesktop.NetworkManager'
+_NM_IFACE = 'org.freedesktop.NetworkManager'
+_NM_PATH = '/org/freedesktop/NetworkManager'
+_NM_DEVICE_IFACE = 'org.freedesktop.NetworkManager.Device'
+_NM_WIRELESS_IFACE = 'org.freedesktop.NetworkManager.Device.Wireless'
+_NM_ACCESSPOINT_IFACE = 'org.freedesktop.NetworkManager.AccessPoint'
+_NM_ACTIVE_CONN_IFACE = 'org.freedesktop.NetworkManager.Connection.Active'
+
+
+_adhoc_manager_instance = None
+def get_adhoc_manager_instance():
+    global _adhoc_manager_instance
+    if _adhoc_manager_instance is None:
+        _adhoc_manager_instance = AdHocManager()
+    return _adhoc_manager_instance
+
+
+class AdHocManager(gobject.GObject):
+    """To mimic the mesh behavior on devices where mesh hardware is
+    not available we support the creation of an Ad-hoc network on
+    three channels 1, 6, 11. If Sugar sees no "known" network when it
+    starts, it does autoconnect to an Ad-hoc network.
+
+    """
+
+    __gsignals__ = {
+        'members-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE,
+                            ([gobject.TYPE_PYOBJECT, gobject.TYPE_PYOBJECT])),
+        'state-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE,
+                          ([gobject.TYPE_PYOBJECT, gobject.TYPE_PYOBJECT]))
+    }
+
+    _AUTOCONNECT_TIMEOUT = 30
+    _CHANNEL_1 = 1
+    _CHANNEL_6 = 6
+    _CHANNEL_11 = 11
+
+    def __init__(self):
+        gobject.GObject.__init__(self)
+
+        self._bus = dbus.SystemBus()
+        self._device = None
+        self._idle_source = 0
+        self._listening_called = 0
+        self._device_state = network.DEVICE_STATE_UNKNOWN
+
+        self._current_channel = None
+        self._networks = {self._CHANNEL_1: None,
+                          self._CHANNEL_6: None,
+                          self._CHANNEL_11: None}
+
+    def start_listening(self, device):
+        self._listening_called += 1
+        if self._listening_called > 1:
+            raise RuntimeError('The start listening method can' \
+                                   ' only be called once.')
+
+        self._device = device
+        props = dbus.Interface(device,
+                                   'org.freedesktop.DBus.Properties')
+        props.Get(_NM_DEVICE_IFACE, 'State',
+                  reply_handler=self.__get_device_state_reply_cb,
+                  error_handler=self.__get_state_error_cb)
+
+        self._bus.add_signal_receiver(self.__device_state_changed_cb,
+                                      signal_name='StateChanged',
+                                      path=self._device.object_path,
+                                      dbus_interface=_NM_DEVICE_IFACE)
+
+        self._bus.add_signal_receiver(self.__wireless_properties_changed_cb,
+                                      signal_name='PropertiesChanged',
+                                      path=self._device.object_path,
+                                      dbus_interface=_NM_WIRELESS_IFACE)
+
+    def stop_listening(self):
+        self._bus.remove_signal_receiver(self.__device_state_changed_cb,
+                                         signal_name='StateChanged',
+                                         path=self._device.object_path,
+                                         dbus_interface=_NM_DEVICE_IFACE)
+        self._bus.remove_signal_receiver(self.__wireless_properties_changed_cb,
+                                         signal_name='PropertiesChanged',
+                                         path=self._device.object_path,
+                                         dbus_interface=_NM_WIRELESS_IFACE)
+
+    def __get_state_error_cb(self, err):
+        logging.debug('Error getting the device state: %s', err)
+
+    def __get_device_state_reply_cb(self, state):
+        self._device_state = state
+
+    def __device_state_changed_cb(self, new_state, old_state, reason):
+        self._device_state = new_state
+        self._update_state()
+
+    def __wireless_properties_changed_cb(self, properties):
+        if 'ActiveAccessPoint' in properties and \
+                properties['ActiveAccessPoint'] != '/':
+            active_ap = self._bus.get_object(_NM_SERVICE,
+                                             properties['ActiveAccessPoint'])
+            props = dbus.Interface(active_ap, dbus.PROPERTIES_IFACE)
+            props.GetAll(_NM_ACCESSPOINT_IFACE, byte_arrays=True,
+                         reply_handler=self.__get_all_ap_props_reply_cb,
+                         error_handler=self.__get_all_ap_props_error_cb)
+
+    def __get_all_ap_props_reply_cb(self, properties):
+        if properties['Mode'] == network.NM_802_11_MODE_ADHOC and \
+                'Frequency' in properties:
+            frequency = properties['Frequency']
+            self._current_channel = network.frequency_to_channel(frequency)
+        else:
+            self._current_channel = None
+        self._update_state()
+
+    def __get_all_ap_props_error_cb(self, err):
+        logging.error('Error getting the access point properties: %s', err)
+
+    def _update_state(self):
+        self.emit('state-changed', self._current_channel, self._device_state)
+
+    def _have_configured_connections(self):
+        return len(network.get_settings().connections) > 0
+
+    def autoconnect(self):
+        """Autoconnect to an Ad-hoc network"""
+        if self._have_configured_connections():
+            self._autoconnect_adhoc_timer()
+        else:
+            self._autoconnect_adhoc()
+
+    def _autoconnect_adhoc_timer(self):
+        """Start a timer which basically looks for 30 seconds of inactivity
+        on the device, then does autoconnect to an Ad-hoc network.
+
+        """
+        if self._idle_source != 0:
+            gobject.source_remove(self._idle_source)
+        self._idle_source = gobject.timeout_add_seconds( \
+                self._AUTOCONNECT_TIMEOUT, self.__idle_check_cb)
+
+    def __idle_check_cb(self):
+        if  self._device_state == network.DEVICE_STATE_DISCONNECTED:
+            logging.debug("Connect to Ad-hoc network due to inactivity.")
+            self._autoconnect_adhoc()
+        return False
+
+    def _autoconnect_adhoc(self):
+        """First we try if there is an Ad-hoc network that is used by other
+        learners in the area, if not we default to channel 1.
+
+        """
+        if self._networks[self._CHANNEL_1] is not None:
+            self._connect(self._CHANNEL_1)
+        elif self._networks[self._CHANNEL_6] is not None:
+            self._connect(self._CHANNEL_6)
+        elif self._networks[self._CHANNEL_11] is not None:
+            self._connect(self._CHANNEL_11)
+        else:
+            self._connect(self._CHANNEL_1)
+
+    def activate_channel(self, channel):
+        """Activate a sugar Ad-hoc network.
+
+        Keyword arguments:
+        channel -- Channel to connect to (should be 1, 6, 11)
+
+        """
+        self._connect(channel)
+
+    def _connect(self, channel):
+        name = "Ad-hoc Network %d" % channel
+        connection = network.find_connection_by_ssid(name)
+        if connection is None:
+            settings = Settings()
+            settings.connection.id = name
+            settings.connection.uuid = unique_id()
+            settings.connection.type = '802-11-wireless'
+            settings.wireless.ssid = dbus.ByteArray(name)
+            settings.wireless.band = 'bg'
+            settings.wireless.channel = channel
+            settings.wireless.mode = 'adhoc'
+            settings.ip4_config = IP4Config()
+            settings.ip4_config.method = 'link-local'
+
+            connection = network.add_connection(name, settings)
+
+        obj = self._bus.get_object(_NM_SERVICE, _NM_PATH)
+        netmgr = dbus.Interface(obj, _NM_IFACE)
+
+        netmgr.ActivateConnection(network.SETTINGS_SERVICE,
+                                  connection.path,
+                                  self._device.object_path,
+                                  '/',
+                                  reply_handler=self.__activate_reply_cb,
+                                  error_handler=self.__activate_error_cb)
+
+    def deactivate_active_channel(self):
+        """Deactivate the current active channel."""
+        obj = self._bus.get_object(_NM_SERVICE, _NM_PATH)
+        netmgr = dbus.Interface(obj, _NM_IFACE)
+
+        netmgr_props = dbus.Interface(netmgr, dbus.PROPERTIES_IFACE)
+        netmgr_props.Get(_NM_IFACE, 'ActiveConnections', \
+                reply_handler=self.__get_active_connections_reply_cb,
+                error_handler=self.__get_active_connections_error_cb)
+
+    def __get_active_connections_reply_cb(self, active_connections_o):
+        for connection_o in active_connections_o:
+            obj = self._bus.get_object(_NM_IFACE, connection_o)
+            props = dbus.Interface(obj, dbus.PROPERTIES_IFACE)
+            state = props.Get(_NM_ACTIVE_CONN_IFACE, 'State')
+            if state == network.NM_ACTIVE_CONNECTION_STATE_ACTIVATED:
+                access_point_o = props.Get(_NM_ACTIVE_CONN_IFACE,
+                                           'SpecificObject')
+                if access_point_o != '/':
+                    obj = self._bus.get_object(_NM_SERVICE, _NM_PATH)
+                    netmgr = dbus.Interface(obj, _NM_IFACE)
+                    netmgr.DeactivateConnection(connection_o)
+
+    def __get_active_connections_error_cb(self, err):
+        logging.error('Error getting the active connections: %s', err)
+
+    def __activate_reply_cb(self, connection):
+        logging.debug('Ad-hoc network created: %s', connection)
+
+    def __activate_error_cb(self, err):
+        logging.error('Failed to create Ad-hoc network: %s', err)
+
+    def add_access_point(self, access_point):
+        """Add an access point to a network and notify the view to idicate
+        the member change.
+
+        Keyword arguments:
+        access_point -- Access Point
+
+        """
+        if access_point.name.endswith(' 1'):
+            self._networks[self._CHANNEL_1] = access_point
+            self.emit('members-changed', self._CHANNEL_1, True)
+        elif access_point.name.endswith(' 6'):
+            self._networks[self._CHANNEL_6] = access_point
+            self.emit('members-changed', self._CHANNEL_6, True)
+        elif access_point.name.endswith('11'):
+            self._networks[self._CHANNEL_11] = access_point
+            self.emit('members-changed', self._CHANNEL_11, True)
+
+    def is_sugar_adhoc_access_point(self, ap_object_path):
+        """Checks whether an access point is part of a sugar Ad-hoc network.
+
+        Keyword arguments:
+        ap_object_path -- Access Point object path
+
+        Return: Boolean
+
+        """
+        for access_point in self._networks.values():
+            if access_point is not None:
+                if access_point.model.object_path == ap_object_path:
+                    return True
+        return False
+
+    def remove_access_point(self, ap_object_path):
+        """Remove an access point from a sugar Ad-hoc network.
+
+        Keyword arguments:
+        ap_object_path -- Access Point object path
+
+        """
+        for channel in self._networks:
+            if self._networks[channel] is not None:
+                if self._networks[channel].model.object_path == ap_object_path:
+                    self.emit('members-changed', channel, False)
+                    self._networks[channel] = None
+                    break

data/sugar.schemas.in
@@ -329,5 +329,19 @@
       </locale>
     </schema>
 
+    <schema>
+      <key>/schemas/desktop/sugar/network/adhoc</key>
+      <applyto>/desktop/sugar/network/adhoc</applyto>
+      <owner>sugar</owner>
+      <type>bool</type>
+      <default>true</default>
+      <locale name="C">
+        <short>Show Sugar Ad-hoc networks</short>
+        <long>If TRUE, Sugar will show default Ad-hoc networks for
+	  channel 1,6 and 11. If Sugar sees no "known" network when
+	  it starts, it does autoconnect to an Ad-hoc network.</long>
+      </locale>
+    </schema>
+
   </schemalist>
 </gconfschemafile>

extensions/deviceicon/network.py
@@ -68,24 +68,16 @@ _GSM_STATE_CONNECTING = 2
 _GSM_STATE_CONNECTED = 3
 _GSM_STATE_NEED_AUTH = 4
 
-def frequency_to_channel(frequency):
-    ftoc = { 2412: 1, 2417: 2, 2422: 3, 2427: 4,
-             2432: 5, 2437: 6, 2442: 7, 2447: 8,
-             2452: 9, 2457: 10, 2462: 11, 2467: 12,
-             2472: 13}
-    return ftoc[frequency]
 
 class WirelessPalette(Palette):
     __gtype_name__ = 'SugarWirelessPalette'
 
     __gsignals__ = {
         'deactivate-connection' : (gobject.SIGNAL_RUN_FIRST,
-                                   gobject.TYPE_NONE, ([])),
-        'create-connection'     : (gobject.SIGNAL_RUN_FIRST,
-                                   gobject.TYPE_NONE, ([])),
+                                   gobject.TYPE_NONE, ([]))
     }
 
-    def __init__(self, primary_text, can_create=True):
+    def __init__(self, primary_text):
         Palette.__init__(self, label=primary_text)
 
         self._disconnect_item = None
@@ -118,12 +110,6 @@ class WirelessPalette(Palette):
         self._disconnect_item.connect('activate', self.__disconnect_activate_cb)
         self.menu.append(self._disconnect_item)
 
-        if can_create:
-            self._adhoc_item = gtk.MenuItem(_('Create new wireless network'))
-            self._adhoc_item.connect('activate', self.__adhoc_activate_cb)
-            self.menu.append(self._adhoc_item)
-            self._adhoc_item.show()
-
     def set_connecting(self):
         self.props.secondary_text = _('Connecting...')
 
@@ -150,14 +136,8 @@ class WirelessPalette(Palette):
     def __disconnect_activate_cb(self, menuitem):
         self.emit('deactivate-connection')
 
-    def __adhoc_activate_cb(self, menuitem):
-        self.emit('create-connection')
-
     def _set_frequency(self, frequency):
-        try:
-            channel = frequency_to_channel(frequency)
-        except KeyError:
-            channel = 0
+        channel = network.frequency_to_channel(frequency)
         self._set_channel(channel)
 
     def _set_channel(self, channel):
@@ -319,7 +299,6 @@ class GsmPalette(Palette):
 
 class WirelessDeviceView(ToolButton):
 
-    _ICON_NAME = 'network-wireless'
     FRAME_POSITION_RELATIVE = 302
 
     def __init__(self, device):
@@ -338,7 +317,7 @@ class WirelessDeviceView(ToolButton):
         self._active_ap_op = None
 
         self._icon = PulsingIcon()
-        self._icon.props.icon_name = get_icon_state(self._ICON_NAME, 0)
+        self._icon.props.icon_name = get_icon_state('network-wireless', 0)
         self._inactive_color = xocolor.XoColor( \
             "%s,%s" % (style.COLOR_BUTTON_GREY.get_svg(),
                        style.COLOR_TRANSPARENT.get_svg()))
@@ -352,8 +331,6 @@ class WirelessDeviceView(ToolButton):
         self._palette = WirelessPalette(self._name)
         self._palette.connect('deactivate-connection',
                               self.__deactivate_connection_cb)
-        self._palette.connect('create-connection',
-                              self.__create_connection_cb)
         self.set_palette(self._palette)
         self._palette.set_group_id('frame')
 
@@ -423,11 +400,6 @@ class WirelessDeviceView(ToolButton):
     def __ap_properties_changed_cb(self, properties):
         self._update_properties(properties)
 
-    def _name_encodes_colors(self):
-        """Match #XXXXXX,#YYYYYY at the end of the network name"""
-        return self._name[-7] == '#' and self._name[-8] == ',' \
-            and self._name[-15] == '#'
-
     def _update_properties(self, properties):
         if 'Mode' in properties:
             self._mode = properties['Mode']
@@ -443,11 +415,9 @@ class WirelessDeviceView(ToolButton):
             self._frequency = properties['Frequency']
 
         if self._color == None:
-            if self._mode == network.NM_802_11_MODE_ADHOC \
-                    and self._name_encodes_colors():
-                encoded_color = self._name.split("#", 1)
-                if len(encoded_color) == 2:
-                    self._color = xocolor.XoColor('#' + encoded_color[1])
+            if self._mode == network.NM_802_11_MODE_ADHOC and \
+                    network.is_sugar_adhoc_network(self._name):
+                self._color = profile.get_color()
             else:
                 sha_hash = hashlib.sha1()
                 data = self._name + hex(self._flags)
@@ -483,14 +453,24 @@ class WirelessDeviceView(ToolButton):
         else:
             state = network.DEVICE_STATE_UNKNOWN
 
-        if state == network.DEVICE_STATE_ACTIVATED:
-            icon_name = '%s-connected' % self._ICON_NAME
-        else:
-            icon_name = self._ICON_NAME
+        if self._mode != network.NM_802_11_MODE_ADHOC and \
+                network.is_sugar_adhoc_network(self._name) == False:
+            if state == network.DEVICE_STATE_ACTIVATED:
+                icon_name = '%s-connected' % 'network-wireless'
+            else:
+                icon_name = 'network-wireless'
 
-        icon_name = get_icon_state(icon_name, self._strength)
-        if icon_name:
-            self._icon.props.icon_name = icon_name
+            icon_name = get_icon_state(icon_name, self._strength)
+            if icon_name:
+                self._icon.props.icon_name = icon_name
+        else:
+            channel = network.frequency_to_channel(self._frequency)
+            if state == network.DEVICE_STATE_ACTIVATED:
+                self._icon.props.icon_name = 'network-adhoc-%s-connected' \
+                        % channel
+            else:
+                self._icon.props.icon_name = 'network-adhoc-%s' % channel
+            self._icon.props.base_color = profile.get_color()
 
         if state == network.DEVICE_STATE_PREPARE or \
            state == network.DEVICE_STATE_CONFIG or \
@@ -528,54 +508,6 @@ class WirelessDeviceView(ToolButton):
                 if ap_op == self._active_ap_op:
                     netmgr.DeactivateConnection(conn_o)
                     break
-
-    def __create_connection_cb(self, palette, data=None):
-        """Create an 802.11 IBSS network.
-
-        The user's color is encoded at the end of the network name. The network
-        name is truncated so that it does not exceed the 32 byte SSID limit.
-        """
-        client = gconf.client_get_default()
-        nick = client.get_string('/desktop/sugar/user/nick').decode('utf-8')
-        color = client.get_string('/desktop/sugar/user/color')
-        color_suffix = ' %s' % color
-
-        format = _('%s\'s network').encode('utf-8')
-        extra_length = (len(format) - len('%s')) + len(color_suffix)
-        name_limit = 32 - extra_length
-
-        # truncate the nick and use a regex to drop any partial characters
-        # at the end
-        nick = nick.encode('utf-8')[:name_limit]
-        pattern = "([\xf6-\xf7][\x80-\xbf]{0,2}|[\xe0-\xef][\x80-\xbf]{0,1}|[\xc0-\xdf])$"
-        nick = re.sub(pattern, '', nick)
-
-        connection_name = format % nick
-        connection_name += color_suffix
-
-        connection = network.find_connection_by_ssid(connection_name)
-        if connection is None:
-            settings = Settings()
-            settings.connection.id = 'Auto ' + connection_name
-            uuid = settings.connection.uuid = unique_id()
-            settings.connection.type = '802-11-wireless'
-            settings.wireless.ssid = dbus.ByteArray(connection_name)
-            settings.wireless.band = 'bg'
-            settings.wireless.mode = 'adhoc'
-            settings.ip4_config = IP4Config()
-            settings.ip4_config.method = 'link-local'
-
-            connection = network.add_connection(uuid, settings)
-
-        obj = self._bus.get_object(_NM_SERVICE, _NM_PATH)
-        netmgr = dbus.Interface(obj, _NM_IFACE)
-
-        netmgr.ActivateConnection(network.SETTINGS_SERVICE,
-                                  connection.path,
-                                  self._device.object_path,
-                                  '/',
-                                  reply_handler=self.__activate_reply_cb,
-                                  error_handler=self.__activate_error_cb)
 
     def __activate_reply_cb(self, connection):
         logging.debug('Network created: %s', connection)

commit 8e93940d088b572575c83bb5f158d2675a1cfece

Fix source ID not found when ad hoc fails
When a system has no wireless firmware, but does have a wireless device,
or when a wireless device does not support ad hoc independent basic
service set (IBSS), the logs contain;

jarabe/model/adhoc.py:152: Warning: Source ID 135 was not found when attempting to remove it
  GLib.source_remove(self._idle_source)

Cause is expiry and execution of the idle callback leaving the source
identifier still present in the variable.

Fixed by clearing the variable.

Regression introduced by 5ff30eadf670f5225ae305f10e248c7d97a88fb4 when
Sugar did not support systems like this.

Signed-off-by: James Cameron <quozl@laptop.org>

src/jarabe/model/adhoc.py
@@ -159,6 +159,7 @@ class AdHocManager(GObject.GObject):
             self._autoconnect_adhoc()
         else:
             logging.debug('autoconnect Sugar Ad-hoc: already connected')
+        self._idle_source = 0
         return False
 
     def _autoconnect_adhoc(self):