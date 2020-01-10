fc922438560a29c3d78be490b1c37f750b17732d
Srevin Saju <srevin03@gmail.com>
2020-01-10 10:13:50 +0300

    Important Bugfix extra function

--- src/jarabe/main.py
+++ src/jarabe/main.py
@@ -195,12 +195,9 @@ def _restart_window_manager():
     return False
 
 
-def _restore_gnome_cursor():
-
-
 def _start_window_manager():
-    settings = Gio.Settings.new('org.gnome.desktop.interface')
     global _saved_cursor_theme
+    settings = Gio.Settings.new('org.gnome.desktop.interface')
     _saved_cursor_theme = settings.get_string('cursor-theme')
 
     settings.set_string('cursor-theme', 'sugar')
