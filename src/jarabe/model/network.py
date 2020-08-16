sugarlabs/sugar

commit aa18879e9717dfe2d30f249549e9a43d6dd6da4f

Port to Python 3
* remove call to sys.setdefaultencoding
  default encoding is 'utf-8' by default in Python 3

* remove use of statvfs
  statvfs does not exist in python 3

* TODO was fixed in af6ce2e

* Remove unused function

* flake fixes

* Fix unicode encoding error

* Use a list to iterate over, supporting Python 3

* Encode 'str' instance into 'utf-8'

Co-authored-by: Aniket21mathur <aniketmathur320@gmail.com>

src/jarabe/model/network.py
@@ -25,7 +25,7 @@ import uuid
 import dbus
 import dbus.service
 from gi.repository import GObject
-import ConfigParser
+import configparser
 from gi.repository import Gio
 import ctypes
 
@@ -701,7 +701,7 @@ class AccessPoint(GObject.GObject):
         else:
             fl |= 1 << 6
 
-        hashstr = str(fl) + '@' + self.ssid
+        hashstr = str(fl) + '@' + self.ssid.decode()
         return hash(hashstr)
 
     def _update_properties(self, properties):
@@ -957,12 +957,12 @@ def _migrate_old_wifi_connections():
     if not os.path.exists(config_path):
         return
 
-    config = ConfigParser.ConfigParser()
+    config = configparser.ConfigParser()
     try:
         if not config.read(config_path):
             logging.error('Error reading the nm config file')
             return
-    except ConfigParser.ParsingError:
+    except configparser.ParsingError:
         logging.exception('Error reading the nm config file')
         return
 
@@ -1005,7 +1005,7 @@ def _migrate_old_wifi_connections():
                     if config.has_option(section, 'pairwise'):
                         value = config.get(section, 'pairwise')
                         settings.wireless_security.pairwise = value
-        except ConfigParser.Error:
+        except configparser.Error:
             logging.exception('Error reading section')
         else:
             add_connection(settings)
@@ -1095,7 +1095,7 @@ def _is_non_printable(char):
     """
     Return True if char is a non-printable unicode character, False otherwise
     """
-    return (char < u' ') or (u'~' < char < u'\xA0') or (char == u'\xAD')
+    return (char < ' ') or ('~' < char < '\xA0') or (char == '\xAD')
 
 
 def ssid_to_display_name(ssid):
@@ -1123,7 +1123,7 @@ def ssid_to_display_name(ssid):
     """
     for encoding in ['utf-8', 'iso-8859-1', 'windows-1251']:
         try:
-            display_name = unicode(ssid, encoding)
+            display_name = str(ssid, encoding)
         except UnicodeDecodeError:
             continue
 

extensions/deviceicon/network.py
@@ -515,8 +515,8 @@ class WirelessDeviceView(ToolButton):
                 self._color = profile.get_color()
             else:
                 sha_hash = hashlib.sha1()
-                data = self._ssid + hex(self._flags)
-                sha_hash.update(data)
+                data = self._ssid.decode() + hex(self._flags)
+                sha_hash.update(data.encode('utf-8'))
                 digest = hash(sha_hash.digest())
                 index = digest % len(xocolor.colors)
 

commit 7024fc6d9b287c902c21013d0311aef775793809

Use bytes for ssid
 Use bytes for ssid Dbus.ByteArray takes encoded bytes i.e. <class bytes> as an argument in Python3.
 Documentation<https://dbus.freedesktop.org/doc/dbus-python/PY3PORT.html> says ' ByteArray objects must be initialized with bytes objects, not unicodes. Use b’’ literals in the constructor.'
 ConfigParser's get method returns <str> object.
 Encode str to bytes before using it in ByteArrays.
 Regression introduced in https://github.com/sugarlabs/sugar/commit/aa18879e9717dfe2d30f249549e9a43d6dd6da4f ("Port to Python 3")

src/jarabe/model/network.py
@@ -970,7 +970,7 @@ def _migrate_old_wifi_connections():
         try:
             settings = Settings()
             settings.connection.id = section
-            ssid = config.get(section, 'ssid')
+            ssid = config.get(section, 'ssid').encode()
             settings.wireless.ssid = dbus.ByteArray(ssid)
             config_uuid = config.get(section, 'uuid')
             settings.connection.uuid = config_uuid