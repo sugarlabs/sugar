55f90d5b42102a423aa0233bc9416ad23aa9094c
Ayush Tripathi <tripathiayush1986@gmail.com>
2026-03-23 14:57:53 +0530

    Fix indentation + duplicate assignment in MeshBox

--- src/jarabe/desktop/meshbox.py
+++ src/jarabe/desktop/meshbox.py
@@ -429,36 +429,37 @@ class MeshBox(ViewContainer):
             icon.set_filter(self._query)
 
         self._buddies[buddy_model.props.key] = icon
-def _remove_buddy(self, buddy_model):
-    logging.debug('MeshBox._remove_buddy')
 
-    key = buddy_model.props.key
-    icon = self._buddies.get(key)
+    def _remove_buddy(self, buddy_model):
+        logging.debug('MeshBox._remove_buddy')
 
-    if not icon:
-        logging.warning(f"Buddy key {key} not found during removal")
-        return
+        key = buddy_model.props.key
+        icon = self._buddies.get(key)
 
-    self.remove(icon)
-    del self._buddies[key]
+        if not icon:
+            logging.warning(f"Buddy key {key} not found during removal")
+            return
 
-def __buddy_notify_current_activity_cb(self, buddy_model, pspec):
-key = buddy_model.props.key
-activity = buddy_model.props.current_activity
-logging.debug(f"[STATE] key={key}, activity={activity}, exists={key in self._buddies}")
+        self.remove(icon)
+        del self._buddies[key]
 
-if activity is None:
-    if key not in self._buddies:
-        logging.debug(f"Adding buddy {key}")
-        self._add_buddy(buddy_model)
-    else:
-        logging.debug(f"Buddy {key} already exists, skipping add")
-else:
-    if key in self._buddies:
-        logging.debug(f"Removing buddy {key}")
-        self._remove_buddy(buddy_model)
-    else:
-        logging.debug(f"Buddy {key} not found, skipping remove")
+    def __buddy_notify_current_activity_cb(self, buddy_model, pspec):
+        key = buddy_model.props.key
+        activity = buddy_model.props.current_activity
+        logging.debug(f"[STATE] key={key}, activity={activity}, exists={key in self._buddies}")
+
+        if activity is None:
+            if key not in self._buddies:
+                logging.debug(f"Adding buddy {key}")
+                self._add_buddy(buddy_model)
+            else:
+                logging.debug(f"Buddy {key} already exists, skipping add")
+        else:
+            if key in self._buddies:
+                logging.debug(f"Removing buddy {key}")
+                self._remove_buddy(buddy_model)
+            else:
+                logging.debug(f"Buddy {key} not found, skipping remove")
 
     def _add_activity(self, activity_model):
         icon = ActivityView(activity_model)
